84af96ecb19bea348f056bb122f98cace6bb90d8
CZ <zheng.chen@no-trade-no.life>
2023-08-29 00:53:27 +0800

    feat(host): init host app

---
+++ apps/host/src/index.ts
@@ -0,0 +1,69 @@
+import { createServer } from 'http';
+import { Observable, bindCallback, fromEvent, merge } from 'rxjs';
+import WebSocket from 'ws';
+
+const mapTerminalIdToSocket: Record<string, WebSocket.WebSocket> = {};
+
+const server = createServer();
+
+const wss = new WebSocket.Server({
+  noServer: true,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3,
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024,
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024, // Size (in bytes) below which messages
+    // should not be compressed if context takeover is disabled.
+  },
+});
+
+merge(
+  bindCallback(process.once).call(process, 'SIGINT'),
+  bindCallback(process.once).call(process, 'SIGTERM'),
+).subscribe((sig) => {
+  console.info(new Date(), sig, 'terminate signal received, gracefully shutting down');
+  // ISSUE: 关闭所有连接，提早终端感知到并重连
+  for (const ws of wss.clients) {
+    ws.close();
+  }
+  wss.close();
+  server.close();
+  console.info(new Date(), 'GracefullyShutdown', 'Done clean up');
+  process.exit(0);
+});
+
+server.on('upgrade', (request, socket, head) => {
+  const url = new URL(request.url || '', 'http://localhost:8888');
+  const params = url.searchParams;
+  const host_token = params.get('host_token');
+  const terminal_id = params.get('terminal_id');
+  if (!((!process.env.HOST_TOKEN || host_token === process.env.HOST_TOKEN) && terminal_id)) {
+    console.info(new Date(), 'Auth Failed', { terminal_id, host_token });
+    socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+    socket.destroy();
+    return;
+  }
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    console.info(new Date(), 'New Connection', { terminal_id });
+    mapTerminalIdToSocket[terminal_id]?.close(); // 关闭旧连接
+    mapTerminalIdToSocket[terminal_id] = ws; // 注册终端
+    // 配置消息转发
+    (fromEvent(ws, 'message') as Observable<WebSocket.MessageEvent>).subscribe((origin) =>
+      mapTerminalIdToSocket[JSON.parse(origin.data.toString()).target_terminal_id]?.send(origin.data),
+    );
+  });
+});
+
+server.listen(8888);
---
+++ apps/host/src/manifests/index.ts
@@ -0,0 +1,191 @@
+import { IDeploySpec, IEnvContext, makeDockerEnvs, makeK8sEnvs } from '@yuants/protocol';
+import { JSONSchema7 } from 'json-schema';
+
+export const make_json_schema = (): JSONSchema7 => {
+  return {
+    type: 'object',
+    title: 'Host',
+    properties: {
+      env: {
+        type: 'object',
+        properties: {
+          HOST_TOKEN: { type: 'string', title: 'for client authentication' },
+        },
+      },
+      network: {
+        type: 'object',
+        properties: {
+          backward_proxy: {
+            type: 'object',
+            properties: {
+              host: { type: 'string' },
+            },
+          },
+          port_forward: {
+            type: 'object',
+            properties: {
+              host: { type: 'number' },
+            },
+          },
+        },
+      },
+    },
+  };
+};
+
+export const make_docker_compose_file = async (ctx: IDeploySpec, envCtx: IEnvContext) => ({
+  host: {
+    image: `ghcr.io/no-trade-no-life/host:${ctx.version ?? envCtx.version}`,
+    ports: [['host', 8888]]
+      .filter(([name]) => ctx.network?.port_forward?.[name] !== undefined)
+      .map(([name, targetPort]) => `${ctx.network!.port_forward![name]}:${targetPort}`),
+    environment: makeDockerEnvs(ctx.env),
+  },
+});
+
+export const make_k8s_resource_objects = async (ctx: IDeploySpec, envCtx: IEnvContext) => {
+  return {
+    deployment: {
+      apiVersion: 'apps/v1',
+      kind: 'Deployment',
+      metadata: {
+        name: 'host',
+        namespace: 'yuan',
+        labels: {
+          'y.ntnl.io/version': ctx.version ?? envCtx.version,
+          'y.ntnl.io/component': 'host',
+        },
+      },
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: {
+            'y.ntnl.io/component': 'host',
+          },
+        },
+        template: {
+          metadata: {
+            annotations: {},
+            labels: {
+              'y.ntnl.io/version': ctx.version ?? envCtx.version,
+              'y.ntnl.io/component': 'host',
+            },
+          },
+          spec: {
+            imagePullSecrets: [
+              {
+                name: 'pull-secret', // TODO(wsy): such things should be managed at namespace scope
+              },
+            ],
+            containers: [
+              {
+                name: 'host',
+                image: `ghcr.io/no-trade-no-life/host:${ctx.version ?? envCtx.version}`,
+                imagePullPolicy: 'IfNotPresent',
+                env: makeK8sEnvs(ctx.env),
+                ports: [
+                  {
+                    name: 'host',
+                    containerPort: 8888,
+                    protocol: 'TCP',
+                  },
+                ],
+                resources: {
+                  limits: {
+                    cpu: ctx.cpu?.max ?? '400m',
+                    memory: ctx.memory?.max ?? '512Mi',
+                  },
+                  requests: {
+                    cpu: ctx.cpu?.min ?? '100m',
+                    memory: ctx.memory?.min ?? '128Mi',
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+    service: {
+      apiVersion: 'v1',
+      kind: 'Service',
+      metadata: {
+        name: 'host',
+        namespace: 'yuan',
+        labels: {
+          'y.ntnl.io/version': ctx.version ?? envCtx.version,
+          'y.ntnl.io/component': 'host',
+        },
+      },
+      spec: {
+        type: 'ClusterIP',
+        ports: ['host']
+          .map((name) => ({
+            port: ctx.network?.port_forward?.[name],
+            targetPort: name,
+            name,
+            protocol: 'TCP',
+          }))
+          .filter(({ port }) => port != undefined),
+        selector: {
+          'y.ntnl.io/component': 'host',
+        },
+      },
+    },
+    ingress: {
+      apiVersion: 'networking.k8s.io/v1',
+      kind: 'Ingress',
+      metadata: {
+        name: 'host',
+        namespace: 'yuan',
+        annotations: {
+          'cert-manager.io/cluster-issuer': 'letsencrypt-prod', // TODO(wsy): make this a dependent value
+          // 'nginx.ingress.kubernetes.io/auth-signin': `https://${
+          //   ctx.annotations!.sso_url
+          // }/oauth2/start?rd=$scheme://$best_http_host$request_uri`,
+          // 'nginx.ingress.kubernetes.io/auth-url': `https://${ctx.annotations!.sso_url}/oauth2/auth`
+        },
+        labels: {
+          'y.ntnl.io/version': ctx.version ?? envCtx.version,
+          'y.ntnl.io/component': 'host',
+        },
+      },
+      spec: {
+        ingressClassName: 'nginx',
+        rules: ['host']
+          .filter(
+            (v) =>
+              ctx.network?.backward_proxy?.[v] !== undefined && ctx.network?.port_forward?.[v] !== undefined,
+          )
+          .map((name) => {
+            const parts = new URL(ctx.network!.backward_proxy![name]);
+            return {
+              host: parts.host!,
+              http: {
+                paths: [
+                  {
+                    path: parts.pathname,
+                    pathType: 'Prefix',
+                    backend: {
+                      service: {
+                        name,
+                        port: {
+                          number: ctx.network!.port_forward![name],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            };
+          }),
+        tls: [
+          {
+            hosts: Object.values(ctx.network!.backward_proxy ?? {}).map((v) => new URL(v).host),
+            secretName: 'host-tls',
+          },
+        ],
+      },
+    },
+  };
+};
