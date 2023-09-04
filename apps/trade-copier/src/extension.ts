1e3c9a4ab172c3f5b9284be0376f81589b363b1d
Siyuan Wang <c.one@thrimbda.com>
2023-09-04 16:04:07 +0800

    fix(trade-copier): alerting for lost connection

--- apps/trade-copier/src/extension.ts
+++ apps/trade-copier/src/extension.ts
@@ -138,10 +138,10 @@ export default (context: IExtensionContext) => {
                       runbook_url: 'https://tradelife.feishu.cn/wiki/wikcnwLKfcKGaIaAZhasEXM7ipc',
                       summary: 'Trade Copier Metrics 请求失败',
                     },
-                    expr: 'rate(hv_terminal_metric_fetch_errors_total{terminal_id=~"^trade-copier/.+"}[5m]) != 0',
+                    expr: 'rate(terminal_metrics_fetch_errors_total{terminal_id="TradeCopier"}[5m]) != 0',
                     for: '5m',
                     labels: {
-                      severity: 'error',
+                      severity: 'critical',
                     },
                   },
                   {
