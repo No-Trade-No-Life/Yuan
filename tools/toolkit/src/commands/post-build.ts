c0c682797b55f7e1f5aa625dec1769329d8e0195
Zheng Chen <zheng.chen@no-trade-no.life>
2023-09-06 23:32:00 +0800

    refactor(toolkit): merge dependency-hash and docker-bake into toolkit (#109)
    * refactor(toolkit): merge dependency-hash and docker-bake into toolkit
    
    * fix(ctp): update package dependency

--- tools/toolkit/src/commands/post-build.ts
+++ tools/toolkit/src/commands/post-build.ts
@@ -1,10 +1,14 @@
 import { Command } from 'clipanion';
+import { buildDependencyHash } from '../features/buildDependencyHash';
+import { buildDockerImage } from '../features/buildDockerImage';
 import { buildExtensionBundle } from '../features/buildExtensionBundle';
 
 export class PostBuildCommand extends Command {
   static paths = [['post-build']];
 
   async execute(): Promise<number | void> {
+    await buildDependencyHash();
+    await buildDockerImage();
     await buildExtensionBundle();
 
     return 0;
