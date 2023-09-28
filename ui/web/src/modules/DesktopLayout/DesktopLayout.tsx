No-Trade-No-Life/Yuan

commit 98c74fdd75518e7e68ab2fdc6068ce8b11f9bff5

refactor(web): refactor for page and layout (#157)
* refactor(web): remove react-router

* refactor(web): rename openSingletonComponent to openPage

* refactor(web): hide concepts of flexlayout-react from specific pages

* refactor(web): naively extract module 'DesktopLayout' and 'Pages'

ui/web/src/modules/DesktopLayout/DesktopLayout.tsx
@@ -1,21 +1,57 @@
 import { Layout, Space, Typography } from '@douyinfe/semi-ui';
 import { Actions, Layout as FlexLayout, TabNode } from 'flexlayout-react';
 import { useObservableState } from 'observable-hooks';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { isDarkMode$ } from '../../common/Darkmode';
 import { ErrorBoundary } from '../../common/ErrorBoundary';
-import { layoutModel$, layoutModelJson$ } from '../../layout-model';
 import { CommandCenter } from '../CommandCenter/CommandCenter';
 import { NetworkStatusWidget } from '../Terminals/NetworkStatusWidget';
 import { Login } from '../User/Login';
 import { UserMenu } from '../User/UserMenu';
-import { HomePage } from './HomePage';
-import { NotFound } from './NotFound';
+import { HomePage } from '../Workbench/HomePage';
+import { NotFound } from '../Workbench/NotFound';
+import { layoutModel$, layoutModelJson$ } from './layout-model';
 const AvailableComponents: Record<string, React.ComponentType> = {};
 
 export const registerComponent = (components: Record<string, React.ComponentType>) => {
   Object.assign(AvailableComponents, components);
+};
+
+const TabNodeContext = React.createContext<TabNode | null>(null);
+
+export const usePageParams = () => {
+  const node = useContext(TabNodeContext);
+  return node?.getConfig() ?? {};
+};
+
+export const usePageTitle = (title: string) => {
+  const node = useContext(TabNodeContext);
+
+  useEffect(() => {
+    if (node) {
+      node.getModel().doAction(Actions.renameTab(node.getId(), title));
+    }
+  }, [title, node]);
+};
+
+export const usePageType = () => {
+  const node = useContext(TabNodeContext);
+  return node?.getComponent() ?? '';
+};
+
+export const usePageViewport = () => {
+  const node = useContext(TabNodeContext);
+  const rect = node?.getRect();
+  if (rect) {
+    return {
+      x: rect.x,
+      y: rect.y,
+      w: rect.width,
+      h: rect.height,
+    };
+  }
+  return undefined;
 };
 
 // ISSUE: React.memo will cause layout tab label not change while change language
@@ -32,7 +68,11 @@ export const DesktopLayout = () => {
       }
     };
     const theNode = getNode();
-    return <ErrorBoundary>{theNode ? React.cloneElement(theNode, { node }) : null}</ErrorBoundary>;
+    return (
+      <ErrorBoundary>
+        <TabNodeContext.Provider value={node}>{theNode}</TabNodeContext.Provider>
+      </ErrorBoundary>
+    );
   };
 
   const isDarkMode = useObservableState(isDarkMode$);

common/config/rush/pnpm-lock.yaml
@@ -700,7 +700,6 @@ importers:
       react: ^18.2.0
       react-dom: ^18.2.0
       react-i18next: ~13.2.1
-      react-router-dom: ^6.3.0
       rxjs: ^7.5.6
       typescript: ^4.6.4
       unzipit: ~1.4.3
@@ -745,7 +744,6 @@ importers:
       react: 18.2.0
       react-dom: 18.2.0_react@18.2.0
       react-i18next: 13.2.1_98cade5ad3a97019d15195744e997f78
-      react-router-dom: 6.15.0_react-dom@18.2.0+react@18.2.0
       rxjs: 7.5.7
       unzipit: 1.4.3
       uuid: 9.0.0
@@ -2354,11 +2352,6 @@ packages:
       mz: 2.7.0
       write-file-atomic: 3.0.3
       write-yaml-file: 4.2.0
-    dev: false
-
-  /@remix-run/router/1.8.0:
-    resolution: {integrity: sha512-mrfKqIHnSZRyIzBcanNJmVQELTnX+qagEDlcKO90RgRBVOZGSGvZKeDihTRfWcqoDn5N/NkUcwWTccnpN18Tfg==}
-    engines: {node: '>=14.0.0'}
     dev: false
 
   /@rjsf/core/5.1.0_@rjsf+utils@5.1.0+react@18.2.0:
@@ -8776,29 +8769,6 @@ packages:
       react: 18.2.0
       react-dom: 18.2.0_react@18.2.0
       react-draggable: 4.4.5_react-dom@18.2.0+react@18.2.0
-    dev: false
-
-  /react-router-dom/6.15.0_react-dom@18.2.0+react@18.2.0:
-    resolution: {integrity: sha512-aR42t0fs7brintwBGAv2+mGlCtgtFQeOzK0BM1/OiqEzRejOZtpMZepvgkscpMUnKb8YO84G7s3LsHnnDNonbQ==}
-    engines: {node: '>=14.0.0'}
-    peerDependencies:
-      react: '>=16.8'
-      react-dom: '>=16.8'
-    dependencies:
-      '@remix-run/router': 1.8.0
-      react: 18.2.0
-      react-dom: 18.2.0_react@18.2.0
-      react-router: 6.15.0_react@18.2.0
-    dev: false
-
-  /react-router/6.15.0_react@18.2.0:
-    resolution: {integrity: sha512-NIytlzvzLwJkCQj2HLefmeakxxWHWAP+02EGqWEZy+DgfHHKQMUoBBjUQLOtFInBMhWtb3hiUy6MfFgwLjXhqg==}
-    engines: {node: '>=14.0.0'}
-    peerDependencies:
-      react: '>=16.8'
-    dependencies:
-      '@remix-run/router': 1.8.0
-      react: 18.2.0
     dev: false
 
   /react-sortable-hoc/2.0.0_react-dom@18.2.0+react@18.2.0:

ui/web/src/App.tsx
@@ -1,6 +1,5 @@
 import '@icon-park/react/styles/index.css';
 import * as _kernel from '@yuants/kernel';
-import { Route, Routes } from 'react-router-dom';
 import * as rx from 'rxjs';
 import './common/page';
 import { LUI } from './modules/AI/LUI';
@@ -12,6 +11,7 @@ import { AgentConfForm } from './modules/Agent/AgentConfForm';
 import { AccountFrameChart } from './modules/Chart/AccountFrameChart';
 import { TechnicalChart } from './modules/Chart/TechnicalChart';
 import { DeployConfigForm } from './modules/Deploy/DeployConfigForm';
+import { DesktopLayout } from './modules/DesktopLayout';
 import { FileEditor } from './modules/Editor/FileEditor';
 import { ExtensionPanel } from './modules/Extensions/ExtensionPanel';
 import { FinancialStatementsPanel } from './modules/Fund/FinancialStatementsPanel';
@@ -22,6 +22,7 @@ import i18n from './modules/Locale/i18n';
 import { Market } from './modules/Market';
 import { ManualTradePanel } from './modules/Order/ManualTradePanel';
 import { OrderListPanel } from './modules/Order/OrderListPanel';
+import { registerComponent } from './modules/Pages';
 import { ProductList } from './modules/Products/ProductList';
 import { PullSourceRelationList } from './modules/PullSourceRelations/PullSourceRelationList';
 import { RecordTablePanel } from './modules/Shell/RecordTablePanel';
@@ -30,7 +31,6 @@ import { TerminalList } from './modules/Terminals/TerminalList';
 import { TradeConfigList } from './modules/TradeCopier/TradeConfigList';
 import { TradeCopyRelationList } from './modules/TradeCopier/TradeCopyRelationList';
 import { About } from './modules/Workbench/About';
-import { DesktopLayout, registerComponent } from './modules/Workbench/DesktopLayout';
 import { HostList } from './modules/Workbench/HostList';
 import { Program } from './modules/Workbench/Program';
 import { Explorer } from './modules/Workspace/Explorer';
@@ -69,17 +69,8 @@ registerComponent({
   TradeCopyRelationList,
 });
 
-function AppLayout() {
-  return <DesktopLayout />;
-}
-
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}></Route>
-      <Route path="/popout" element={<div />}></Route>
-    </Routes>
-  );
+  return <DesktopLayout />;
 };
 
 export default App;

ui/web/src/assets/react.svg
@@ -1,1 +1,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--logos" width="35.93" height="32" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 228"><path fill="#00D8FF" d="M210.483 73.824a171.49 171.49 0 0 0-8.24-2.597c.465-1.9.893-3.777 1.273-5.621c6.238-30.281 2.16-54.676-11.769-62.708c-13.355-7.7-35.196.329-57.254 19.526a171.23 171.23 0 0 0-6.375 5.848a155.866 155.866 0 0 0-4.241-3.917C100.759 3.829 77.587-4.822 63.673 3.233C50.33 10.957 46.379 33.89 51.995 62.588a170.974 170.974 0 0 0 1.892 8.48c-3.28.932-6.445 1.924-9.474 2.98C17.309 83.498 0 98.307 0 113.668c0 15.865 18.582 31.778 46.812 41.427a145.52 145.52 0 0 0 6.921 2.165a167.467 167.467 0 0 0-2.01 9.138c-5.354 28.2-1.173 50.591 12.134 58.266c13.744 7.926 36.812-.22 59.273-19.855a145.567 145.567 0 0 0 5.342-4.923a168.064 168.064 0 0 0 6.92 6.314c21.758 18.722 43.246 26.282 56.54 18.586c13.731-7.949 18.194-32.003 12.4-61.268a145.016 145.016 0 0 0-1.535-6.842c1.62-.48 3.21-.974 4.76-1.488c29.348-9.723 48.443-25.443 48.443-41.52c0-15.417-17.868-30.326-45.517-39.844Zm-6.365 70.984c-1.4.463-2.836.91-4.3 1.345c-3.24-10.257-7.612-21.163-12.963-32.432c5.106-11 9.31-21.767 12.459-31.957c2.619.758 5.16 1.557 7.61 2.4c23.69 8.156 38.14 20.213 38.14 29.504c0 9.896-15.606 22.743-40.946 31.14Zm-10.514 20.834c2.562 12.94 2.927 24.64 1.23 33.787c-1.524 8.219-4.59 13.698-8.382 15.893c-8.067 4.67-25.32-1.4-43.927-17.412a156.726 156.726 0 0 1-6.437-5.87c7.214-7.889 14.423-17.06 21.459-27.246c12.376-1.098 24.068-2.894 34.671-5.345a134.17 134.17 0 0 1 1.386 6.193ZM87.276 214.515c-7.882 2.783-14.16 2.863-17.955.675c-8.075-4.657-11.432-22.636-6.853-46.752a156.923 156.923 0 0 1 1.869-8.499c10.486 2.32 22.093 3.988 34.498 4.994c7.084 9.967 14.501 19.128 21.976 27.15a134.668 134.668 0 0 1-4.877 4.492c-9.933 8.682-19.886 14.842-28.658 17.94ZM50.35 144.747c-12.483-4.267-22.792-9.812-29.858-15.863c-6.35-5.437-9.555-10.836-9.555-15.216c0-9.322 13.897-21.212 37.076-29.293c2.813-.98 5.757-1.905 8.812-2.773c3.204 10.42 7.406 21.315 12.477 32.332c-5.137 11.18-9.399 22.249-12.634 32.792a134.718 134.718 0 0 1-6.318-1.979Zm12.378-84.26c-4.811-24.587-1.616-43.134 6.425-47.789c8.564-4.958 27.502 2.111 47.463 19.835a144.318 144.318 0 0 1 3.841 3.545c-7.438 7.987-14.787 17.08-21.808 26.988c-12.04 1.116-23.565 2.908-34.161 5.309a160.342 160.342 0 0 1-1.76-7.887Zm110.427 27.268a347.8 347.8 0 0 0-7.785-12.803c8.168 1.033 15.994 2.404 23.343 4.08c-2.206 7.072-4.956 14.465-8.193 22.045a381.151 381.151 0 0 0-7.365-13.322Zm-45.032-43.861c5.044 5.465 10.096 11.566 15.065 18.186a322.04 322.04 0 0 0-30.257-.006c4.974-6.559 10.069-12.652 15.192-18.18ZM82.802 87.83a323.167 323.167 0 0 0-7.227 13.238c-3.184-7.553-5.909-14.98-8.134-22.152c7.304-1.634 15.093-2.97 23.209-3.984a321.524 321.524 0 0 0-7.848 12.897Zm8.081 65.352c-8.385-.936-16.291-2.203-23.593-3.793c2.26-7.3 5.045-14.885 8.298-22.6a321.187 321.187 0 0 0 7.257 13.246c2.594 4.48 5.28 8.868 8.038 13.147Zm37.542 31.03c-5.184-5.592-10.354-11.779-15.403-18.433c4.902.192 9.899.29 14.978.29c5.218 0 10.376-.117 15.453-.343c-4.985 6.774-10.018 12.97-15.028 18.486Zm52.198-57.817c3.422 7.8 6.306 15.345 8.596 22.52c-7.422 1.694-15.436 3.058-23.88 4.071a382.417 382.417 0 0 0 7.859-13.026a347.403 347.403 0 0 0 7.425-13.565Zm-16.898 8.101a358.557 358.557 0 0 1-12.281 19.815a329.4 329.4 0 0 1-23.444.823c-7.967 0-15.716-.248-23.178-.732a310.202 310.202 0 0 1-12.513-19.846h.001a307.41 307.41 0 0 1-10.923-20.627a310.278 310.278 0 0 1 10.89-20.637l-.001.001a307.318 307.318 0 0 1 12.413-19.761c7.613-.576 15.42-.876 23.31-.876H128c7.926 0 15.743.303 23.354.883a329.357 329.357 0 0 1 12.335 19.695a358.489 358.489 0 0 1 11.036 20.54a329.472 329.472 0 0 1-11 20.722Zm22.56-122.124c8.572 4.944 11.906 24.881 6.52 51.026c-.344 1.668-.73 3.367-1.15 5.09c-10.622-2.452-22.155-4.275-34.23-5.408c-7.034-10.017-14.323-19.124-21.64-27.008a160.789 160.789 0 0 1 5.888-5.4c18.9-16.447 36.564-22.941 44.612-18.3ZM128 90.808c12.625 0 22.86 10.235 22.86 22.86s-10.235 22.86-22.86 22.86s-22.86-10.235-22.86-22.86s10.235-22.86 22.86-22.86Z"></path></svg>

ui/web/src/index.css
@@ -10,11 +10,3 @@ body,
   margin: 0;
   padding: 0;
 }
-
-#root .flexlayout__tabset-selected {
-  background-image: none;
-}
-
-body[theme-mode='dark'] .flexlayout__tab {
-  background-color: #141414;
-}

ui/web/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
 );

ui/web/src/modules/AI/LUI.tsx
@@ -1,15 +1,14 @@
 import { IconDelete, IconSend, IconUser } from '@douyinfe/semi-icons';
 import { Avatar, Button, Space, TextArea } from '@douyinfe/semi-ui';
 import { format } from 'date-fns';
-import { TabNode } from 'flexlayout-react';
 import { useObservableState } from 'observable-hooks';
 import React, { useState } from 'react';
 import { authState$ } from '../../common/supabase';
 import { createPersistBehaviorSubject } from '../../common/utils';
-import { openSingletonComponent } from '../../layout-model';
 import { agentConf$ } from '../Agent/AgentConfForm';
 import { registerCommand } from '../CommandCenter/CommandCenter';
 import { fs } from '../FileSystem/api';
+import { openPage, usePageViewport } from '../Pages';
 import { triggerLoginModalAction$ } from '../User/Login';
 import './LUI.css';
 
@@ -31,11 +30,12 @@ const pushHistoryMessages = (...messages: IMessage[]) => {
 };
 
 registerCommand('AI', () => {
-  openSingletonComponent('LUI');
+  openPage('LUI');
 });
 
-export const LUI = React.memo((props: { node?: TabNode }) => {
-  const width = props.node?.getRect().width ?? Infinity;
+export const LUI = React.memo(() => {
+  const viewport = usePageViewport();
+  const width = viewport?.w ?? Infinity;
   const [isLoading, setLoading] = useState(false);
 
   const authState = useObservableState(authState$);

ui/web/src/modules/AccountInfo/AccountInfoItem.tsx
@@ -1,14 +1,12 @@
 import { Button, Descriptions, List, Space, Spin, Typography } from '@douyinfe/semi-ui';
 import { formatTime } from '@yuants/kernel';
-import { Actions, DockLocation } from 'flexlayout-react';
 import { useObservableState } from 'observable-hooks';
 import React from 'react';
 import { useAccountInfo } from '../../common/source';
-import { layoutModel$ } from '../../layout-model';
+import { executeCommand } from '../CommandCenter/CommandCenter';
 
 export const AccountInfoItem = React.memo((props: { account_id: string }) => {
   const accountInfo = useObservableState(useAccountInfo(props.account_id));
-  const model = useObservableState(layoutModel$);
   const timeLag = Date.now() - (accountInfo?.timestamp_in_us ?? NaN) / 1000;
   return (
     <List.Item>
@@ -40,27 +38,7 @@ export const AccountInfoItem = React.memo((props: { account_id: string }) => {
         <Button
           onClick={() => {
             const account_id = props.account_id;
-            const nodeId = `AccountInfo/${props.account_id}`;
-            const node = model.getNodeById(nodeId);
-
-            if (node) {
-              model.doAction(Actions.selectTab(node.getId()));
-            } else {
-              model.doAction(
-                Actions.addNode(
-                  {
-                    id: `AccountInfo/${account_id}`,
-                    type: 'tab',
-                    component: 'AccountInfoPanel',
-                    name: `账户详情 ${account_id}`,
-                    config: { account_id: account_id },
-                  },
-                  '#main',
-                  DockLocation.CENTER,
-                  0,
-                ),
-              );
-            }
+            executeCommand('AccountInfoPanel', { account_id });
           }}
         >
           详情

commit d80add08a1b1118bb837fb06fe2817cbbccd351b

fix(web): Infinite Render Loop caused by #157

ui/web/src/modules/DesktopLayout/DesktopLayout.tsx
@@ -27,12 +27,15 @@ export const usePageParams = () => {
 
 export const usePageTitle = (title: string) => {
   const node = useContext(TabNodeContext);
+  const nodeId = node?.getId();
 
+  // ISSUE: node cannot be treat as deps. Or it will cause infinite rendering
+  // (node change -> rename tab -> layout change -> node change -> ...)
   useEffect(() => {
-    if (node) {
-      node.getModel().doAction(Actions.renameTab(node.getId(), title));
+    if (nodeId) {
+      layoutModel$.value.doAction(Actions.renameTab(nodeId, title));
     }
-  }, [title, node]);
+  }, [title, nodeId]);
 };
 
 export const usePageType = () => {