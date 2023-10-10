4cda71fe5998c39f48860061dd6205d51c2f10fd
Siyuan Wang <c.one@thrimbda.com>
2023-10-11 02:06:56 +0800

    feat: add docs site as home page

---
+++ ui/docs/src/pages/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+import Translate, { translate } from '@docusaurus/Translate';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to="https://y.ntnl.io">
+            <Translate id="home.getStartedButton">{'Get Started'}</Translate>
+          </Link>
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            <Translate id="home.readTheDocsButton">{'Read The Docs'}</Translate>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={translate({ message: 'home.welcomeTitle' }, { value: siteConfig.title })}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
