25e9414a5d4bd86b0643c5eb07271bb250a1565a
CZ <zheng.chen@no-trade-no.life>
2023-12-09 19:50:56 +0800

    amend

--- ui/web/src/modules/Terminals/TerminalList.tsx
+++ ui/web/src/modules/Terminals/TerminalList.tsx
@@ -1,12 +1,12 @@
 import { List } from '@douyinfe/semi-ui';
 import { useObservableState } from 'observable-hooks';
-import { shareReplay, switchMap } from 'rxjs';
+import { of, shareReplay, switchMap } from 'rxjs';
 import { registerPage } from '../Pages';
 import { TerminalListItem } from './TerminalListItem';
 import { terminal$ } from './create-connection';
 
 export const terminalList$ = terminal$.pipe(
-  switchMap((terminal) => terminal.terminalInfos$),
+  switchMap((terminal) => terminal?.terminalInfos$ ?? of([])),
   shareReplay(1),
 );
 
