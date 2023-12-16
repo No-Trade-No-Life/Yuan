991070a89355691c48b0a88590489dc1f84b47c3
Siyuan Wang <c.one@thrimbda.com>
2023-12-17 02:54:23 +0800

    feat(ui): add youtube video card & error boundary (#374)

--- ui/web/src/modules/Copilot/components/UserText.tsx
+++ ui/web/src/modules/Copilot/components/UserText.tsx
@@ -2,9 +2,23 @@ import { Typography } from '@douyinfe/semi-ui';
 import { IMessageCardProps } from '../model';
 
 export default ({ payload }: IMessageCardProps<{ text: string }>) => {
+  const youtube_url_matched = /youtube\.com\/watch\?v=(.+)/.exec(payload.text);
   return (
-    <Typography.Title heading={3} style={{ width: '100%', flexShrink: 0 }}>
-      {payload.text}
-    </Typography.Title>
+    <>
+      <Typography.Title heading={3} style={{ width: '100%', flexShrink: 0 }}>
+        {payload.text}
+      </Typography.Title>
+      {youtube_url_matched?.[1] && (
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${youtube_url_matched[1]}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      )}
+    </>
   );
 };
