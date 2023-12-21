a9699a6618088eb468abaf1d8e684cb86f895504
Jinhaolin <542538131@qq.com>
2023-12-22 02:53:08 +0800

    feat: add video && seo (#392)
    * add video && seo
    
    * rush update
    
    ---------
    
    Co-authored-by: Zheng Chen <zheng.chen@no-trade-no.life>

---
+++ ui/docs/src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Translate from '@docusaurus/Translate';
+import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
+const HeroContent = () => {
+  return (
+    <div className={styles.heroBanner}>
+      <div className={styles.heroContent}>
+        <div className={styles.hero}>
+          <Translate id="home.heroTitle">{'Yuan - The Investment OS For Everyone'}</Translate>
+        </div>
+        <div className={styles.heroSubTitle}>
+          <Translate id="home.heroSubTitle">{'The investment OS sfor everyone'}</Translate>
+        </div>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to="https://y.ntnl.io">
+            <Translate id="home.getStartedButton">{'Launch Now'}</Translate>
+          </Link>
+        </div>
+      </div>
+
+      <iframe
+        className={styles.iframe}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/RR74o1uojVo?si=jaiqj7VDpxFyTai&autoplay=1&mute=1&loop=1"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
+export default HeroContent;
