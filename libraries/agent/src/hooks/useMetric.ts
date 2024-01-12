1c7b9d9db1c059f6e548887cbd89fb35a2d6e615
zccz14 <zheng.chen@no-trade-no.life>
2024-01-12 11:11:33 +0800

    amend

--- libraries/agent/src/hooks/useMetric.ts
+++ libraries/agent/src/hooks/useMetric.ts
@@ -1,6 +1,9 @@
 import { PromRegistry } from '@yuants/protocol';
 import { useEffect, useMemo } from './basic-set';
 
+/**
+ * @public
+ */
 export const useMetric = (name: string, value: number, labels?: Record<string, string>) => {
   const Metric = useMemo(() => PromRegistry.create('gauge', `agent_custom_metric_${name}`), []);
 
