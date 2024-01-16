56db5641a75619cfa6ffd3ae6b54720da120ad50
Zheng Chen <zheng.chen@no-trade-no.life>
2024-01-16 10:41:51 +0800

    feat(gui): Manifest List (#422)

--- ui/web/src/modules/Deploy/ManifestList.tsx
+++ ui/web/src/modules/Deploy/ManifestList.tsx
@@ -1,69 +1,115 @@
-import { Button, Space, Table } from '@douyinfe/semi-ui';
+import { IconDelete, IconRefresh, IconUndo } from '@douyinfe/semi-icons';
+import { Space, Table } from '@douyinfe/semi-ui';
+import { formatTime } from '@yuants/data-model';
+import { IDeploySpec } from '@yuants/extension';
+import { t } from 'i18next';
+import { useObservableState } from 'observable-hooks';
+import { BehaviorSubject, Subject, defer, repeat, shareReplay } from 'rxjs';
+import { executeCommand, registerCommand } from '../CommandCenter';
+import { Button } from '../Interactive';
 import { registerPage } from '../Pages';
 import { supabase } from '../SupaBase';
-import { Subject, defer, repeat, shareReplay } from 'rxjs';
-import { useObservableState } from 'observable-hooks';
-import { IconDelete, IconRefresh } from '@douyinfe/semi-icons';
-import { t } from 'i18next';
-import { executeCommand, registerCommand } from '../CommandCenter';
+import { useEffect } from 'react';
 
-const refreshAction$ = new Subject<void>();
+export interface ISupabaseManifestRecord {
+  id: string;
+  user_id: string;
+  manifest: IDeploySpec;
+  created_at: string;
+  updated_at: string;
+  expired_at: string | null;
+}
 
-const shareManifests$ = defer(async () => {
+const ManifestList$ = new BehaviorSubject<ISupabaseManifestRecord[]>([]);
+
+registerCommand('Manifest.Load', async () => {
   const res = await supabase.from('manifest').select('*');
-  const data: Array<{
-    id: string;
-    content: {};
-    deploy_key: string;
-    host_id: string;
-  }> = res.data || [];
-  return data;
-}).pipe(
-  //
-
-  repeat({ delay: () => refreshAction$ }),
-  shareReplay(1),
-);
+  const data: Array<ISupabaseManifestRecord> = res.data || [];
+  ManifestList$.next(data);
+});
 
 registerCommand('Manifest.Delete', async ({ id }) => {
-  await supabase.from('manifest').delete().eq('id', id);
-  refreshAction$.next();
+  await supabase
+    .from('manifest')
+    .update({ expired_at: formatTime(Date.now()) })
+    .eq('id', id);
+  await executeCommand('Manifest.Load');
+});
+
+registerCommand('Manifest.Recover', async ({ id }) => {
+  await supabase.from('manifest').update({ expired_at: null }).eq('id', id);
+  await executeCommand('Manifest.Load');
 });
 
 registerPage('ManifestList', () => {
-  const data = useObservableState(shareManifests$);
+  const data = useObservableState(ManifestList$);
+
+  useEffect(() => {
+    executeCommand('Manifest.Load');
+  }, []);
 
   return (
     <Space vertical align="start">
       <Space>
-        <Button
-          icon={<IconRefresh />}
-          onClick={() => {
-            refreshAction$.next();
-          }}
-        ></Button>
+        <Button icon={<IconRefresh />} onClick={() => executeCommand('Manifest.Load')}></Button>
       </Space>
       <Table
         dataSource={data}
         columns={[
           //
           {
-            title: 'Deploy Key',
-            render: (_, v) => v.deploy_key,
+            title: 'ID',
+            render: (_, v) => v.id,
+          },
+          {
+            title: 'Package',
+            render: (_, v) => v.manifest.package,
+          },
+          {
+            title: 'Version',
+            render: (_, v) => v.manifest.version,
+          },
+          {
+            title: 'Updated At',
+            render: (_, v) => formatTime(v.updated_at),
+          },
+          {
+            title: 'Created At',
+            render: (_, v) => formatTime(v.created_at),
+          },
+          {
+            title: 'Expired At',
+            render: (_, v) => (v.expired_at ? formatTime(v.expired_at) : '-'),
+          },
+          {
+            title: 'Manifest',
+            width: 300,
+            render: (_, v) => JSON.stringify(v.manifest),
           },
           {
             title: 'Actions',
             render: (_, v) => (
               <Space>
-                <Button
-                  type="danger"
-                  icon={<IconDelete />}
-                  onClick={() => {
-                    executeCommand('Manifest.Delete', { id: v.id });
-                  }}
-                >
-                  {t('common:delete')}
-                </Button>
+                {/* <Button icon={<IconEdit />} onClick={() => executeCommand('Manifest.Edit', { id: v.id })}>
+                  {t('common:edit')}
+                </Button> */}
+                {v.expired_at && (
+                  <Button
+                    icon={<IconUndo />}
+                    onClick={() => executeCommand('Manifest.Recover', { id: v.id })}
+                  >
+                    {t('common:recover')}
+                  </Button>
+                )}
+                {!v.expired_at && (
+                  <Button
+                    type="danger"
+                    icon={<IconDelete />}
+                    onClick={() => executeCommand('Manifest.Delete', { id: v.id })}
+                  >
+                    {t('common:delete')}
+                  </Button>
+                )}
               </Space>
             ),
           },
