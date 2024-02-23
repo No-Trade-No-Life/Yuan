9102cdb9dd57436b4e5f5ab546e71e0274849974
Zheng Chen <zheng.chen@no-trade-no.life>
2024-02-23 16:48:23 +0800

    refactor(gui): GSR List and Manifest List use DataView (#460)

--- ui/web/src/modules/GeneralSpecificRelations/GeneralSpecificRelationList.tsx
+++ ui/web/src/modules/GeneralSpecificRelations/GeneralSpecificRelationList.tsx
@@ -1,10 +1,12 @@
 import { IconCopyAdd, IconDelete, IconEdit, IconRefresh, IconSearch } from '@douyinfe/semi-icons';
-import { Button, Modal, Popconfirm, Space, Table, Toast } from '@douyinfe/semi-ui';
+import { Button, Modal, Popconfirm, Space, Toast } from '@douyinfe/semi-ui';
+import { createColumnHelper, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import { IDataRecord } from '@yuants/protocol';
 import { useObservable, useObservableState } from 'observable-hooks';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { EMPTY, combineLatest, filter, first, mergeMap, tap, toArray } from 'rxjs';
 import Form from '../Form';
+import { DataView } from '../Interactive';
 import { registerPage } from '../Pages';
 import { terminal$ } from '../Terminals';
 
@@ -67,7 +69,13 @@ registerPage('GeneralSpecificRelationList', () => {
             terminal?.queryDataRecords<IGeneralSpecificRelation>({
               type: 'general_specific_relation',
               tags: {},
-              options: {},
+              options: {
+                sort: [
+                  //
+                  ['origin.general_product_id', 1],
+                  ['origin.specific_datasource_id', 1],
+                ],
+              },
             }) ?? EMPTY
           ).pipe(
             //
@@ -90,10 +98,80 @@ registerPage('GeneralSpecificRelationList', () => {
     [searchFormData, refreshId],
   );
 
-  const records = useObservableState(records$);
+  const records = useObservableState(records$, []);
 
   const [isModalVisible, setModalVisible] = useState(false);
   const [formData, setFormData] = useState({} as IGeneralSpecificRelation);
+
+  const columns = useMemo(() => {
+    const columnHelper = createColumnHelper<IDataRecord<IGeneralSpecificRelation>>();
+    return [
+      columnHelper.accessor('origin.general_product_id', {
+        header: () => '标准品种ID',
+      }),
+      columnHelper.accessor('origin.specific_datasource_id', {
+        header: () => '具体数据源ID',
+      }),
+      columnHelper.accessor('origin.specific_product_id', {
+        header: () => '具体品种ID',
+      }),
+      columnHelper.accessor((x) => 0, {
+        id: 'actions',
+        header: () => '操作',
+        cell: (x) => {
+          const record = x.row.original;
+          return (
+            <Space>
+              <Button
+                icon={<IconEdit />}
+                onClick={() => {
+                  setFormData(record.origin);
+
+                  setModalVisible(true);
+                }}
+              ></Button>
+              <Popconfirm
+                style={{ width: 300 }}
+                title="确定是否删除？"
+                content="此操作将不可逆"
+                onConfirm={() => {
+                  {
+                    terminal$
+                      .pipe(
+                        //
+                        filter((x): x is Exclude<typeof x, null> => !!x),
+                        first(),
+                        mergeMap((terminal) =>
+                          terminal.removeDataRecords({
+                            type: 'general_specific_relation',
+                            id: record.id,
+                          }),
+                        ),
+                        tap({
+                          complete: () => {
+                            Toast.success(`成功删除数据记录 ${record.id}`);
+                            setRefreshId((x) => x + 1);
+                          },
+                        }),
+                      )
+                      .subscribe();
+                  }
+                }}
+              >
+                <Button icon={<IconDelete />} type="danger"></Button>
+              </Popconfirm>
+            </Space>
+          );
+        },
+      }),
+    ];
+  }, []);
+
+  const table = useReactTable({
+    columns,
+    data: records,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <Space vertical align="start" style={{ width: '100%' }}>
@@ -125,61 +203,8 @@ registerPage('GeneralSpecificRelationList', () => {
           刷新
         </Button>
       </Space>
-      <Table
-        dataSource={records}
-        style={{ width: '100%' }}
-        columns={[
-          //
-          { title: '标准品种 ID', render: (_, record) => record.origin.general_product_id },
-          { title: '具体品种 ID', render: (_, record) => record.origin.specific_product_id },
-          { title: '具体品种数据源 ID', render: (_, record) => record.origin.specific_datasource_id },
-          {
-            title: '操作',
-            render: (_, record) => (
-              <Space>
-                <Button
-                  icon={<IconEdit />}
-                  onClick={() => {
-                    setFormData(record.origin);
+      <DataView table={table} />
 
-                    setModalVisible(true);
-                  }}
-                ></Button>
-                <Popconfirm
-                  style={{ width: 300 }}
-                  title="确定是否删除？"
-                  content="此操作将不可逆"
-                  onConfirm={() => {
-                    {
-                      terminal$
-                        .pipe(
-                          //
-                          filter((x): x is Exclude<typeof x, null> => !!x),
-                          first(),
-                          mergeMap((terminal) =>
-                            terminal.removeDataRecords({
-                              type: 'general_specific_relation',
-                              id: record.id,
-                            }),
-                          ),
-                          tap({
-                            complete: () => {
-                              Toast.success(`成功删除数据记录 ${record.id}`);
-                              setRefreshId((x) => x + 1);
-                            },
-                          }),
-                        )
-                        .subscribe();
-                    }
-                  }}
-                >
-                  <Button icon={<IconDelete />} type="danger"></Button>
-                </Popconfirm>
-              </Space>
-            ),
-          },
-        ]}
-      ></Table>
       <Modal
         visible={isModalVisible}
         onCancel={() => {
