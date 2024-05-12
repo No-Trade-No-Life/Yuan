1c5876d9e365dc57130cddccfc7d2f27cc8b23ee
CZ <zheng.chen@no-trade-no.life>
2024-05-13 06:17:55 +0800

    feat(gate): Gate API for future

--- apps/vendor-gate/src/api.ts
+++ apps/vendor-gate/src/api.ts
@@ -1,8 +1,4 @@
 import { formatTime } from '@yuants/data-model';
-import '@yuants/protocol/lib/services';
-import '@yuants/protocol/lib/services/order';
-import '@yuants/protocol/lib/services/transfer';
-
 // @ts-ignore
 import CryptoJS from 'crypto-js';
 interface IGateParams {
@@ -55,10 +51,25 @@ export class GateClient {
     try {
       return JSON.parse(retStr);
     } catch (e) {
-      console.error(formatTime(Date.now()), 'huobiRequestFailed', path, JSON.stringify(params), retStr);
+      console.error(formatTime(Date.now()), 'RequestFailed', path, JSON.stringify(params), retStr);
       throw e;
     }
   }
+
+  /**
+   * 获取用户账户信息
+   *
+   * https://www.gate.io/docs/developers/apiv4/zh_CN/#%E8%8E%B7%E5%8F%96%E7%94%A8%E6%88%B7%E8%B4%A6%E6%88%B7%E4%BF%A1%E6%81%AF
+   */
+  getAccountDetail = (): Promise<{
+    user_id: number;
+    ip_whitelist: string[];
+    currency_pairs: string[];
+    key: {
+      mode: number;
+    };
+    tier: number;
+  }> => this.request('GET', '/api/v4/account/detail');
 
   getUnifiedAccounts(params?: { currency?: string }): Promise<{
     user_id: string;
@@ -96,21 +107,14 @@ export class GateClient {
     return this.request('GET', '/api/v4/unified/accounts', params);
   }
 
-  getAccountDetail(): Promise<{
-    user_id: string;
-    tier: number;
-    key: {
-      mode: number;
-    };
-    currency_pairs: string[];
-    ip_whitelist: string[];
-  }> {
-    return this.request('GET', '/api/v4/account/detail');
-  }
-
-  getFuturePositions(
+  /**
+   * 获取用户仓位列表
+   *
+   * https://www.gate.io/docs/developers/apiv4/zh_CN/#%E8%8E%B7%E5%8F%96%E7%94%A8%E6%88%B7%E4%BB%93%E4%BD%8D%E5%88%97%E8%A1%A8
+   */
+  getFuturePositions = (
+    quote_currency: string,
     params?: { holding?: boolean; limit?: number; offset?: number },
-    quote_currency = 'usdt',
   ): Promise<
     {
       user: number;
@@ -128,11 +132,15 @@ export class GateClient {
       realised_pnl: string;
       mode: string;
     }[]
-  > {
-    return this.request('GET', `/api/v4/futures/${quote_currency}/positions`, params);
-  }
+  > => this.request('GET', `/api/v4/futures/${quote_currency}/positions`, params);
 
+  /**
+   * 查询合约订单列表
+   *
+   * https://www.gate.io/docs/developers/apiv4/zh_CN/#%E6%9F%A5%E8%AF%A2%E5%90%88%E7%BA%A6%E8%AE%A2%E5%8D%95%E5%88%97%E8%A1%A8
+   */
   getFuturesOrders(
+    settle: string,
     params: {
       contract?: string;
       status: string;
@@ -140,7 +148,6 @@ export class GateClient {
       offset?: number;
       last_id?: number;
     },
-    quote_currency = 'usdt',
   ): Promise<
     {
       id: string;
@@ -153,8 +160,108 @@ export class GateClient {
       text: string;
     }[]
   > {
-    return this.request('GET', `/api/v4/futures/${quote_currency}/orders`, params);
+    return this.request('GET', `/api/v4/futures/${settle}/orders`, params);
   }
+
+  /**
+   * 获取合约账号
+   *
+   * https://www.gate.io/docs/developers/apiv4/zh_CN/#%E8%8E%B7%E5%8F%96%E5%90%88%E7%BA%A6%E8%B4%A6%E5%8F%B7
+   */
+  getFuturesAccounts = (
+    settle: string,
+  ): Promise<{
+    user: number;
+    currency: string;
+    total: string;
+    unrealised_pnl: string;
+    position_margin: string;
+    order_margin: string;
+    available: string;
+    point: string;
+    bonus: string;
+    in_dual_mode: boolean;
+    enable_evolved_classic: boolean;
+    history: {
+      dnw: string;
+      pnl: string;
+      fee: string;
+      refr: string;
+      fund: string;
+      point_dnw: string;
+      point_fee: string;
+      point_refr: string;
+      bonus_dnw: string;
+      bonus_offset: string;
+    };
+  }> => this.request('GET', `/api/v4/futures/${settle}/accounts`);
+
+  /**
+   * 合约交易下单
+   *
+   * 下单时指定的是合约张数 size ，而非币的数量，每一张合约对应的币的数量是合约详情接口里返回的 quanto_multiplier
+   *
+   * 0 成交的订单在撤单 10 分钟之后无法再获取到，会提到订单不存在
+   *
+   * 设置 reduce_only 为 true 可以防止在减仓的时候穿仓
+   *
+   * 单仓模式下，如果需要平仓，需要设置 size 为 0 ，close 为 true
+   *
+   * 双仓模式下，平仓需要使用 auto_size 来设置平仓方向，并同时设置 reduce_only 为 true，size 为 0
+   *
+   * 设置 stp_act 决定使用限制用户自成交的策略，详细用法参考body参数stp_act
+   *
+   * https://www.gate.io/docs/developers/apiv4/zh_CN/#%E5%90%88%E7%BA%A6%E4%BA%A4%E6%98%93%E4%B8%8B%E5%8D%95
+   */
+  postFutureOrders = (
+    settle: string,
+    params: {
+      contract: string;
+      size: number;
+      iceberg?: number;
+      price?: string;
+      close?: boolean;
+      reduce_only?: boolean;
+      tif?: string;
+      text?: string;
+      auto_size?: string;
+      stp_act?: string;
+    },
+  ): Promise<{
+    label?: string;
+    detail?: string;
+    id: number;
+    user: number;
+    contract: string;
+    create_time: number;
+    size: number;
+    iceberg: number;
+    left: number;
+    price: string;
+    fill_price: string;
+    mkfr: string;
+    tkfr: string;
+    tif: string;
+    refu: number;
+    is_reduce_only: boolean;
+    is_close: boolean;
+    is_liq: boolean;
+    text: string;
+    status: string;
+    finish_time: number;
+    finish_as: string;
+    stp_id: number;
+    stp_act: string;
+    amend_text: string;
+  }> => this.request('POST', `/api/v4/futures/${settle}/orders`, params);
+
+  /**
+   * 撤销单个订单
+   *
+   * https://www.gate.io/docs/developers/apiv4/zh_CN/#%E6%92%A4%E9%94%80%E5%8D%95%E4%B8%AA%E8%AE%A2%E5%8D%95-2
+   */
+  deleteFutureOrders = (settle: string, order_id: string): Promise<{}> =>
+    this.request('DELETE', `/api/v4/futures/${settle}/orders/${order_id}`);
 }
 
 (async () => {
--- apps/vendor-gate/src/index.ts
+++ apps/vendor-gate/src/index.ts
@@ -1,11 +1,9 @@
-import { IAccountMoney, IPosition } from '@yuants/data-model';
+import { IAccountInfo, IOrder, IPosition } from '@yuants/data-model';
 import { Terminal } from '@yuants/protocol';
 import '@yuants/protocol/lib/services';
 import '@yuants/protocol/lib/services/order';
 import '@yuants/protocol/lib/services/transfer';
-
-import { defer, from, map, mergeMap, toArray } from 'rxjs';
-
+import { combineLatest, defer, from, map, mergeMap, repeat, retry, shareReplay, toArray } from 'rxjs';
 import { GateClient } from './api';
 
 (async () => {
@@ -16,15 +14,16 @@ import { GateClient } from './api';
     },
   });
 
-  const gate_account = await client.getUnifiedAccounts({ currency: 'USDT' });
+  const gate_account = await client.getAccountDetail();
   const uid = gate_account.user_id;
-  const account_id = `gate/${uid}`;
+
+  const futureUsdtAccountId = `gate/${uid}/future/usdt`;
 
   const terminal = new Terminal(process.env.HOST_URL!, {
-    terminal_id: process.env.TERMINAL_ID || `gate-client-${account_id}`,
+    terminal_id: process.env.TERMINAL_ID || `@yuants/vendor-gate/${uid}`,
   });
 
-  const accountFuturePosition$ = defer(() => client.getFuturePositions()).pipe(
+  const accountFuturePosition$ = defer(() => client.getFuturePositions('usdt')).pipe(
     //
     mergeMap((res) =>
       from(res).pipe(
@@ -40,8 +39,8 @@ import { GateClient } from './api';
                 : position.size > 0
                 ? 'LONG'
                 : 'SHORT',
-            volume: position.size,
-            free_volume: position.size,
+            volume: Math.abs(position.size),
+            free_volume: Math.abs(position.size),
             position_price: +position.entry_price,
             closable_price: +position.mark_price,
             floating_profit: +position.unrealised_pnl,
@@ -50,34 +49,107 @@ import { GateClient } from './api';
         toArray(),
       ),
     ),
+    repeat({ delay: 1000 }),
+    retry({ delay: 1000 }),
   );
 
-  const accountFutureOpenOrders$ = defer(() => client.getFuturesOrders({ status: 'open' })).pipe(
+  const accountFutureOpenOrders$ = defer(() => client.getFuturesOrders('usdt', { status: 'open' })).pipe(
     //
     mergeMap((res) =>
       from(res).pipe(
-        map((order) => {
+        map((order): IOrder => {
           return {
             order_id: order.id,
-            account_id,
+            account_id: futureUsdtAccountId,
+            submit_at: order.create_time * 1000,
             product_id: order.contract,
             order_type: 'LIMIT',
-            order_direction: order.size > 0 ? 'LONG' : 'SHORT',
+            order_direction:
+              order.size > 0
+                ? order.is_close
+                  ? 'CLOSE_SHORT'
+                  : 'OPEN_LONG'
+                : order.is_close
+                ? 'CLOSE_LONG'
+                : 'OPEN_SHORT',
             volume: Math.abs(order.size),
             price: order.price !== undefined ? +order.price : undefined,
           };
         }),
+        toArray(),
       ),
     ),
+    repeat({ delay: 1000 }),
+    retry({ delay: 1000 }),
   );
 
-  const accountMoney$ = defer(() => client.getUnifiedAccounts({ currency: 'USDT' })).pipe(
-    //
-    map((gate_account): IAccountMoney => {
+  const futureAccount$ = defer(() => client.getFuturesAccounts('usdt')).pipe(
+    repeat({ delay: 1000 }),
+    retry({ delay: 1000 }),
+  );
+
+  const futureUsdtAccountInfo$ = combineLatest([
+    accountFuturePosition$,
+    accountFutureOpenOrders$,
+    futureAccount$,
+  ]).pipe(
+    map(([positions, orders, account]): IAccountInfo => {
+      const free = +account.available;
+      const profit = +account.unrealised_pnl;
+      const balance = +account.total;
+      const equity = balance + profit;
+      const used = equity - free;
+
       return {
-        currency: 'USDT',
-        equity: +gate_account.total,
+        updated_at: Date.now(),
+        timestamp_in_us: Date.now() * 1000,
+        account_id: futureUsdtAccountId,
+        money: {
+          currency: 'USDT',
+          balance,
+          profit,
+          free,
+          used,
+          equity,
+        },
+        positions,
+        orders,
       };
     }),
+    shareReplay(1),
+  );
+
+  terminal.provideAccountInfo(futureUsdtAccountInfo$);
+
+  terminal.provideService(
+    'SubmitOrder',
+    { properties: { account_id: { const: futureUsdtAccountId } } },
+    (msg) =>
+      defer(async () => {
+        const order = msg.req;
+        const res = await client.postFutureOrders('usdt', {
+          contract: order.product_id,
+          size:
+            order.volume *
+            (order.order_direction === 'OPEN_LONG' || order.order_direction === 'CLOSE_SHORT' ? 1 : -1),
+          price: order.order_type === 'MARKET' ? '0' : `${order.price}`,
+          tif: order.order_type === 'MARKET' ? 'ioc' : 'gtc',
+          reduce_only: order.order_direction === 'CLOSE_LONG' || order.order_direction === 'CLOSE_SHORT',
+        });
+        if (res.label && res.detail) {
+          return { res: { code: 400, message: `${res.label}: ${res.detail}` } };
+        }
+        return { res: { code: 0, message: 'OK' } };
+      }),
+  );
+  terminal.provideService(
+    'CancelOrder',
+    { properties: { account_id: { const: futureUsdtAccountId } } },
+    (msg) =>
+      defer(async () => {
+        const order = msg.req;
+        await client.deleteFutureOrders('usdt', order.order_id!);
+        return { res: { code: 0, message: 'OK' } };
+      }),
   );
 })();
