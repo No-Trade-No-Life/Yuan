49e0e370c211f1a1723d58b1af699dfa13f53eab
Zheng Chen <zheng.chen@no-trade-no.life>
2024-05-27 16:24:01 +0800

    feat(gui): Simplify AccountInfoPanel, interest_to_settle, use DataView (#585)

--- ui/web/src/modules/AccountInfo/AccountInfoPanel.tsx
+++ ui/web/src/modules/AccountInfo/AccountInfoPanel.tsx
@@ -1,32 +1,87 @@
-import { IconClose, IconInfoCircle } from '@douyinfe/semi-icons';
-import { Collapse, Descriptions, Empty, Space, Table, Tooltip, Typography } from '@douyinfe/semi-ui';
-import { encodePath, formatTime, mergeAccountInfoPositions } from '@yuants/data-model';
+import { IconClose } from '@douyinfe/semi-icons';
+import { Collapse, Descriptions, Empty, Space, Table, Typography } from '@douyinfe/semi-ui';
+import { createColumnHelper, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import { ITick, encodePath, formatTime, mergeAccountInfoPositions } from '@yuants/data-model';
 import { IPosition } from '@yuants/protocol';
 import { useObservable, useObservableState } from 'observable-hooks';
+import { useMemo } from 'react';
 import {
+  combineLatest,
   defer,
   distinctUntilChanged,
   filter,
+  first,
   firstValueFrom,
   from,
   groupBy,
   map,
   mergeMap,
+  of,
   pairwise,
+  raceWith,
   reduce,
+  shareReplay,
   tap,
+  throttleTime,
+  timer,
   toArray,
 } from 'rxjs';
-import { Button } from '../Interactive';
+import { Button, DataView } from '../Interactive';
 import { registerPage, usePageParams } from '../Pages';
-import { terminal$ } from '../Terminals';
+import { terminal$, useTick } from '../Terminals';
 import { useAccountInfo } from './model';
+
+interface IPositionSummaryItem {
+  product_id: string;
+  long: IPosition;
+  short: IPosition;
+  net: IPosition;
+}
+
+const createColumnsOfPositionSummaryItem = () => {
+  const helper = createColumnHelper<IPositionSummaryItem>();
+  return [
+    helper.accessor('product_id', {
+      header: () => '品种',
+    }),
+    helper.accessor('net.direction', {
+      header: () => '方向',
+    }),
+    helper.accessor('net.valuation', {
+      header: () => '估值',
+    }),
+    helper.accessor('net.volume', {
+      header: () => '持仓量',
+    }),
+    helper.accessor('net.position_price', {
+      header: () => '持仓价',
+    }),
+    helper.accessor('net.closable_price', {
+      header: () => '可平价',
+    }),
+    helper.accessor('net.floating_profit', {
+      header: () => '浮动盈亏',
+    }),
+    helper.accessor('net.interest_to_settle', {
+      header: () => '预计利息',
+    }),
+  ];
+};
+
+const memoizeMap = <T extends (...params: any[]) => any>(fn: T): T => {
+  const cache: Record<string, any> = {};
+  return ((...params: any[]) => (cache[encodePath(params)] ??= fn(...params))) as T;
+};
+
+const useTickMemoized = memoizeMap((datasource_id: string, product_id: string) =>
+  useTick(datasource_id, product_id).pipe(shareReplay(1)),
+);
 
 registerPage('AccountInfoPanel', () => {
   const terminal = useObservableState(terminal$);
   const { account_id: accountId } = usePageParams();
 
-  const accountInfo$ = useObservable(() => useAccountInfo(accountId));
+  const accountInfo$ = useObservable(() => useAccountInfo(accountId).pipe(throttleTime(100)));
 
   const accountInfo = useObservableState(accountInfo$);
 
@@ -74,108 +129,157 @@ registerPage('AccountInfoPanel', () => {
     ),
   );
 
-  if (!accountInfo) {
-    return <Empty title={'加载中'}></Empty>;
-  }
-
-  interface IPositionSummaryItem {
-    product_id: string;
-    long: IPosition;
-    short: IPosition;
-    net: IPosition;
-  }
-
-  let positionSummary: IPositionSummaryItem[] = [];
-  {
-    from(accountInfo.positions)
-      .pipe(
-        groupBy((position) => position.product_id),
-        mergeMap((group) =>
-          group.pipe(
-            groupBy((position) => position.direction),
-            mergeMap((subGroup) =>
-              subGroup.pipe(
-                reduce(
-                  (acc: IPosition, cur: IPosition): IPosition => ({
-                    ...acc,
-                    volume: acc.volume + cur.volume,
-                    free_volume: acc.free_volume + cur.free_volume,
-                    position_price:
-                      (acc.position_price * acc.volume + cur.position_price * cur.volume) /
-                      (acc.volume + cur.volume),
-                    floating_profit: acc.floating_profit + cur.floating_profit,
-                    closable_price:
-                      (acc.closable_price * acc.volume + cur.closable_price * cur.volume) /
-                      (acc.volume + cur.volume),
-                    valuation: acc.valuation + cur.valuation,
-                    comment: '',
+  const positionSummary = useMemo(() => {
+    let positionSummary: IPositionSummaryItem[] = [];
+    {
+      from(accountInfo?.positions || [])
+        .pipe(
+          groupBy((position) => position.product_id),
+          mergeMap((group) =>
+            group.pipe(
+              groupBy((position) => position.direction),
+              mergeMap((subGroup) =>
+                subGroup.pipe(
+                  mergeMap((position) => {
+                    // return of(position);
+                    if (!position.datasource_id) {
+                      return of(position);
+                    }
+                    return combineLatest([
+                      of(position),
+                      useTickMemoized(position.datasource_id, position.product_id).pipe(
+                        raceWith(
+                          timer(5000).pipe(
+                            map(
+                              (x): ITick => ({
+                                datasource_id: position.datasource_id!,
+                                product_id: position.product_id,
+                                updated_at: Date.now(),
+                              }),
+                            ),
+                          ),
+                        ),
+                        first(),
+                      ),
+                    ]).pipe(
+                      map(([position, tick]) => ({
+                        ...position,
+                        settlement_scheduled_at: tick.settlement_scheduled_at,
+                        interest_to_settle:
+                          position.direction === 'LONG'
+                            ? position.valuation * (tick.interest_rate_for_long ?? 0)
+                            : position.direction === 'SHORT'
+                            ? position.valuation * (tick.interest_rate_for_short ?? 0)
+                            : 0,
+                      })),
+                    );
                   }),
+                  reduce(
+                    (acc: IPosition, cur: IPosition): IPosition => ({
+                      ...acc,
+                      volume: acc.volume + cur.volume,
+                      free_volume: acc.free_volume + cur.free_volume,
+                      position_price:
+                        (acc.position_price * acc.volume + cur.position_price * cur.volume) /
+                        (acc.volume + cur.volume),
+                      floating_profit: acc.floating_profit + cur.floating_profit,
+                      closable_price:
+                        (acc.closable_price * acc.volume + cur.closable_price * cur.volume) /
+                        (acc.volume + cur.volume),
+                      valuation: acc.valuation + cur.valuation,
+                      interest_to_settle: (acc.interest_to_settle || 0) + (cur.interest_to_settle || 0),
+                      comment: '',
+                    }),
+                  ),
                 ),
               ),
+              toArray(),
+              map((positions) => {
+                const long: IPosition = positions.find((p) => p.direction === 'LONG') ?? {
+                  position_id: '@long',
+                  product_id: group.key,
+                  volume: 0,
+                  free_volume: 0,
+                  closable_price: 0,
+                  position_price: 0,
+                  floating_profit: 0,
+                  valuation: 0,
+                };
+                const short: IPosition = positions.find((p) => p.direction === 'SHORT') ?? {
+                  position_id: '@short',
+                  product_id: group.key,
+                  volume: 0,
+                  free_volume: 0,
+                  closable_price: 0,
+                  position_price: 0,
+                  floating_profit: 0,
+                  valuation: 0,
+                };
+
+                const net: IPosition = {
+                  position_id: '@net',
+                  product_id: group.key,
+                  direction:
+                    long.volume === short.volume
+                      ? 'LOCKED'
+                      : long.volume - short.volume > 0
+                      ? 'LONG'
+                      : 'SHORT',
+                  volume: Math.abs(long.volume - short.volume),
+                  free_volume: Math.abs(long.free_volume - short.free_volume),
+                  position_price:
+                    (long.position_price * long.volume - short.position_price * short.volume) /
+                    (long.volume - short.volume),
+                  closable_price:
+                    (long.closable_price * long.volume - short.closable_price * short.volume) /
+                    (long.volume - short.volume),
+                  floating_profit: long.floating_profit + short.floating_profit,
+                  valuation: long.valuation + short.valuation,
+                  interest_to_settle: (long.interest_to_settle || 0) + (short.interest_to_settle || 0),
+                };
+
+                const res: IPositionSummaryItem = { product_id: group.key, long, short, net };
+                return res;
+              }),
             ),
-            toArray(),
-            map((positions) => {
-              const long = positions.find((p) => p.direction === 'LONG') ?? {
-                position_id: '@long',
-                product_id: group.key,
-                variant: 'LONG',
-                volume: 0,
-                free_volume: 0,
-                closable_price: 0,
-                position_price: 0,
-                floating_profit: 0,
-                valuation: 0,
-              };
-              const short = positions.find((p) => p.direction === 'SHORT') ?? {
-                position_id: '@short',
-                product_id: group.key,
-                variant: 'LONG',
-                volume: 0,
-                free_volume: 0,
-                closable_price: 0,
-                position_price: 0,
-                floating_profit: 0,
-                valuation: 0,
-              };
-
-              const net: IPosition = {
-                position_id: '@net',
-                product_id: group.key,
-                direction:
-                  long.volume === short.volume ? 'LOCKED' : long.volume - short.volume > 0 ? 'LONG' : 'SHORT',
-                volume: Math.abs(long.volume - short.volume),
-                free_volume: Math.abs(long.free_volume - short.free_volume),
-                position_price:
-                  (long.position_price * long.volume - short.position_price * short.volume) /
-                  (long.volume - short.volume),
-                closable_price:
-                  (long.closable_price * long.volume - short.closable_price * short.volume) /
-                  (long.volume - short.volume),
-                floating_profit: long.floating_profit + short.floating_profit,
-                valuation: long.valuation + short.valuation,
-              };
-
-              const res: IPositionSummaryItem = { product_id: group.key, long, short, net };
-              return res;
-            }),
           ),
-        ),
-        toArray(),
-        map((x) => x.sort((a, b) => b.net.valuation - a.net.valuation)),
-      )
-      .subscribe((_x) => {
-        positionSummary = _x;
-      });
-  }
-  const valuation = accountInfo.positions.reduce((acc, cur) => acc + (cur.valuation || 0), 0);
-  const actual_leverage = accountInfo.money.equity > 0 ? valuation / accountInfo.money.equity : NaN;
+          toArray(),
+          map((x) => x.sort((a, b) => b.net.valuation - a.net.valuation)),
+        )
+        .subscribe((_x) => {
+          positionSummary = _x;
+        });
+    }
+    return positionSummary;
+  }, [accountInfo?.positions]);
 
-  const updatedAt = accountInfo.updated_at!;
+  const valuation = accountInfo?.positions.reduce((acc, cur) => acc + (cur.valuation || 0), 0) ?? 0;
+  const actual_leverage =
+    (accountInfo?.money.equity ?? 0) > 0 ? valuation / (accountInfo?.money.equity ?? 0) : NaN;
+
+  const total_interest_to_settle = positionSummary.reduce(
+    (acc, cur) => acc + (cur.net.interest_to_settle || 0),
+    0,
+  );
+
+  const updatedAt = accountInfo?.updated_at!;
   const renderedAt = Date.now();
 
   const targetTerminalId = Object.entries(terminal?.terminalInfo.subscriptions ?? {}).find(([key, value]) =>
     value.includes(encodePath('AccountInfo', accountId)),
   )?.[0];
+
+  const columns = useMemo(() => createColumnsOfPositionSummaryItem(), []);
+
+  const tableOfPositionSummary = useReactTable({
+    data: positionSummary,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  if (!accountInfo) {
+    return <Empty title={'加载中'}></Empty>;
+  }
 
   return (
     <Space vertical align="start" style={{ padding: '1em', width: '100%' }}>
@@ -226,141 +330,15 @@ registerPage('AccountInfoPanel', () => {
             key: '实际杠杆',
             value: actual_leverage.toFixed(2) + 'x',
           },
-          // {
-          //   key: '账户系统杠杆率',
-          //   value: `${accountInfo.money.leverage ?? 1}x`,
-          // },
-          // {
-          //   key: '实际杠杆率',
-          //   value: `${(
-          //     (accountInfo.money.used / accountInfo.money.equity) *
-          //     (accountInfo.money.leverage ?? 1)
-          //   ).toFixed(2)}x`,
-          // },
-          // {
-          //   key: (
-          //     <Space>
-          //       保证金率{' '}
-          //       <Tooltip content={'保证金率 < 100% 可能会被强制平仓'}>
-          //         <IconInfoCircle />
-          //       </Tooltip>
-          //     </Space>
-          //   ),
-          //   value: `${((accountInfo.money.equity / accountInfo.money.used) * 100).toFixed(2)}%`,
-          // },
+          {
+            key: '总预期利息',
+            value: total_interest_to_settle.toFixed(2) + ' ' + accountInfo.money.currency,
+          },
         ]}
       />
       <Collapse defaultActiveKey={'持仓汇总'} style={{ width: '100%' }}>
         <Collapse.Panel header={`持仓汇总 (${positionSummary.length})`} itemKey="持仓汇总">
-          <Table
-            dataSource={positionSummary}
-            pagination={false}
-            columns={[
-              { title: '持仓品种', render: (_, pos) => pos.product_id },
-              {
-                title: '方向',
-                render: (_, pos) =>
-                  (({ ['LONG']: '多', ['SHORT']: '空' } as any)[pos.net.direction!] || pos.net.direction),
-              },
-              {
-                title: '估值',
-                render: (_, pos) => (
-                  <Space vertical align="start">
-                    <div>净 {+pos.net.valuation?.toFixed(8)}</div>
-                    <div>多 {+pos.long.valuation?.toFixed(8)}</div>
-                    <div>空 {+pos.short.valuation?.toFixed(8)}</div>
-                  </Space>
-                ),
-              },
-              {
-                title: '持仓量',
-                render: (_, pos) => (
-                  <Space vertical align="start">
-                    <div>净 {+pos.net.volume.toFixed(8)}</div>
-                    <div>多 {+pos.long.volume.toFixed(8)}</div>
-                    <div>空 {+pos.short.volume.toFixed(8)}</div>
-                  </Space>
-                ),
-              },
-              {
-                title: '持仓价',
-                render: (_, pos) => (
-                  <Space vertical align="start">
-                    <div>净 {+pos.net.position_price.toFixed(8)}</div>
-                    <div>多 {+pos.long.position_price.toFixed(8)}</div>
-                    <div>空 {+pos.short.position_price.toFixed(8)}</div>
-                  </Space>
-                ),
-              },
-              {
-                title: '可平价',
-                render: (_, pos) => (
-                  <Space vertical align="start">
-                    <div>全平 {+pos.net.closable_price.toFixed(8)}</div>
-                    <div>平多 {+pos.long.closable_price.toFixed(8)}</div>
-                    <div>平空 {+pos.short.closable_price.toFixed(8)}</div>
-                  </Space>
-                ),
-              },
-              {
-                title: '浮动盈亏',
-                render: (_, pos) => (
-                  <Space vertical align="start">
-                    <div>净 {+pos.net.floating_profit.toFixed(8)}</div>
-                    <div>多 {+pos.long.floating_profit.toFixed(8)}</div>
-                    <div>空 {+pos.short.floating_profit.toFixed(8)}</div>
-                  </Space>
-                ),
-              },
-              // ISSUE: 不计算点差，因为无法处理只有单边头寸的情况
-              {
-                title: (
-                  <Space>
-                    预估爆仓价{' '}
-                    <Tooltip
-                      content={'品种价格变化使得净值为零的预估价格，在抵达此价格前可能就会被交易所强制平仓'}
-                    >
-                      <IconInfoCircle />{' '}
-                    </Tooltip>
-                  </Space>
-                ),
-                render: (_, pos) => (
-                  <Space vertical align="start">
-                    <div>
-                      净{' '}
-                      {
-                        +(
-                          pos.net.closable_price -
-                          (accountInfo.money.equity / pos.net.floating_profit) *
-                            (pos.net.closable_price - pos.net.position_price)
-                        ).toFixed(8)
-                      }
-                    </div>
-                    <div>
-                      多{' '}
-                      {
-                        +(
-                          pos.long.closable_price -
-                          (accountInfo.money.equity / pos.long.floating_profit) *
-                            (pos.long.closable_price - pos.long.position_price)
-                        ).toFixed(8)
-                      }
-                    </div>
-                    <div>
-                      空{' '}
-                      {
-                        +(
-                          pos.short.closable_price -
-                          (accountInfo.money.equity / pos.short.floating_profit) *
-                            (pos.short.closable_price - pos.short.position_price)
-                        ).toFixed(8)
-                      }
-                    </div>
-                  </Space>
-                ),
-              },
-            ]}
-          />
+          <DataView table={tableOfPositionSummary} />
         </Collapse.Panel>
         <Collapse.Panel header={`持仓细节 (${accountInfo.positions.length})`} itemKey="持仓细节">
           <Table
