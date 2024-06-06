662bc80ae02ecc2cded2c772aae7689f191b798c
CZ <zheng.chen@no-trade-no.life>
2024-06-06 23:37:24 +0800

    fix(risk-manager): docs

--- apps/risk-manager/src/models/AccountRiskInfo.ts
+++ apps/risk-manager/src/models/AccountRiskInfo.ts
@@ -5,19 +5,43 @@ export interface IAccountRiskInfo {
    * The account ID
    */
   account_id: string;
+  /**
+   * The group ID
+   *
+   * Accounts with the same group ID can transfer to each other
+   */
   group_id: string;
   /**
    * The active supply threshold
    *
    * if equity is greater than this value, the account will actively transfer to the demand-side accounts
    *
-   * The maximum value to transfer is `active_supply_threshold - passive_supply_threshold`
-   *
    * if undefined, active supply is disabled
    */
   active_supply_threshold?: number;
+  /**
+   * The passive supply threshold
+   *
+   * if equity is greater than this value, the account will passively transfer to the demand-side accounts
+   *
+   * if undefined, passive supply is disabled
+   */
   passive_supply_threshold?: number;
+  /**
+   * The active demand threshold
+   *
+   * if equity is less than this value, the account will actively transfer from the supply-side accounts
+   *
+   * if undefined, active demand is disabled
+   */
   active_demand_threshold?: number;
+  /**
+   * The passive demand threshold
+   *
+   * if equity is less than this value, the account will passively transfer from the supply-side accounts
+   *
+   * if undefined, passive demand is disabled
+   */
   passive_demand_threshold?: number;
   /**
    * whether this info is disabled
