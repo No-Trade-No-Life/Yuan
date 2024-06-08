f829b47361a035019d3ddc7b77b180b6f6cfd500
Zheng Chen <zheng.chen@no-trade-no.life>
2024-06-08 17:49:25 +0800

    feat(gui): Account Risk Info List (#620)

---
+++ ui/web/src/modules/AccountRiskInfo/AccountRiskInfoList.tsx
@@ -0,0 +1,65 @@
+import { createColumnHelper } from '@tanstack/react-table';
+import { IDataRecord } from '@yuants/protocol';
+import { InlineAccountId } from '../AccountInfo';
+import { registerCommand } from '../CommandCenter';
+import { DataRecordView } from '../DataRecord';
+import { registerPage } from '../Pages';
+import { terminate } from '../Terminals/TerminalListItem';
+import { IAccountRiskInfo, acrSchema, wrapAccountRiskInfo } from './models/AccountRiskInfo';
+import { Switch } from '@douyinfe/semi-ui';
+
+const TYPE = 'account_risk_info';
+
+function newRecord(): Partial<IAccountRiskInfo> {
+  return {};
+}
+
+function beforeUpdateTrigger(x: IAccountRiskInfo) {}
+
+function defineColumns() {
+  return () => {
+    const columnHelper = createColumnHelper<IDataRecord<IAccountRiskInfo>>();
+    return [
+      columnHelper.accessor('origin.account_id', {
+        header: () => '账户',
+        cell: (ctx) => <InlineAccountId account_id={ctx.getValue()} />,
+      }),
+      columnHelper.accessor('origin.group_id', {
+        header: () => '风险组',
+      }),
+      columnHelper.accessor('origin.active_demand_threshold', {
+        header: () => '主动需求阈值',
+      }),
+      columnHelper.accessor('origin.passive_demand_threshold', {
+        header: () => '被动需求阈值',
+      }),
+      columnHelper.accessor('origin.passive_supply_threshold', {
+        header: () => '被动供给阈值',
+      }),
+      columnHelper.accessor('origin.active_supply_threshold', {
+        header: () => '主动供给阈值',
+      }),
+      columnHelper.accessor('origin.disabled', {
+        header: () => '启用',
+        cell: (ctx) => <Switch checked={!ctx.getValue()} />,
+      }),
+    ];
+  };
+}
+
+registerPage('AccountRiskInfoList', () => {
+  return (
+    <DataRecordView
+      TYPE={TYPE}
+      schema={acrSchema}
+      columns={defineColumns()}
+      newRecord={newRecord}
+      beforeUpdateTrigger={beforeUpdateTrigger}
+      mapOriginToDataRecord={wrapAccountRiskInfo}
+    />
+  );
+});
+
+registerCommand('RiskManager.Restart', () => {
+  terminate('RiskManager');
+});
