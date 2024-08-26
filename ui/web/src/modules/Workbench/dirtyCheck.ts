d1e41808d268072645f176ebfddcf961e59eee71
CZ <zheng.chen@no-trade-no.life>
2024-08-26 22:39:30 +0800

    feat: usePageClosingConfirm hook in Workbench module

--- ui/web/src/modules/Workbench/dirtyCheck.ts
+++ ui/web/src/modules/Workbench/dirtyCheck.ts
@@ -1,13 +1,25 @@
-import { BehaviorSubject } from 'rxjs';
+import { useEffect } from 'react';
 
-export const isDirty = new BehaviorSubject(false);
-
-window.onbeforeunload = function (e) {
-  if (isDirty.value) {
-    // Cancel the event
-    e.preventDefault();
-
-    // Chrome requires returnValue to be set
-    e.returnValue = '?';
-  }
-};
+/**
+ * Hook to use the page closing confirm
+ *
+ * usePageClosingConfirm hook is used to show a confirmation dialog when the user tries to close/refresh the page.
+ *
+ * recommend to use this hook in the component where you want to show the confirmation dialog.
+ *
+ * for example, some component with complex form data that the user might lose if they close the page.
+ */
+export const usePageClosingConfirm = (disabled = false) =>
+  useEffect(() => {
+    if (disabled) return;
+    const listener = (e: BeforeUnloadEvent): void => {
+      // Cancel the event
+      e.preventDefault();
+      // Chrome requires returnValue to be set
+      e.returnValue = '?';
+    };
+    window.addEventListener('beforeunload', listener);
+    return () => {
+      window.removeEventListener('beforeunload', listener);
+    };
+  }, [disabled]);
