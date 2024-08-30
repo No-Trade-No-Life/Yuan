7df43ad373273469854df4539fd83577a8740c5b
CZ <zheng.chen@no-trade-no.life>
2024-08-30 17:46:13 +0800

    feat(utils): add ED25519 sign algorithm utils

--- ui/web/src/modules/Extensions/loadLocalExtension.ts
+++ ui/web/src/modules/Extensions/loadLocalExtension.ts
@@ -1,6 +1,7 @@
 import * as SemiUI from '@douyinfe/semi-ui';
 import * as IconParksIcons from '@icon-park/react';
 import * as dataModel from '@yuants/data-model';
+import * as yuantsUtils from '@yuants/utils';
 import { formatTime } from '@yuants/data-model';
 import * as protocol from '@yuants/protocol';
 import * as observableHooks from 'observable-hooks';
@@ -24,16 +25,21 @@ registerCommand('LoadLocalExtensions', async () => {
         rxjs
           .defer(async () => {
             const code = await bundleCode(x.main);
-            const module = new Function('globals', `return ${code}`)({
+            const requireContext = {
               '@yuants/ui-web': Modules,
               '@yuants/data-model': dataModel,
               '@yuants/protocol': protocol,
+              '@yuants/utils': yuantsUtils,
               react: React,
               rxjs,
               '@douyinfe/semi-ui': SemiUI,
               '@icon-park/react': IconParksIcons,
               'observable-hooks': observableHooks,
-            });
+            };
+
+            Object.assign(globalThis, { requireContext });
+
+            const module = new Function('globals', `return ${code}`)(requireContext);
             return module;
           })
           .pipe(
