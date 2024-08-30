b6ab8cfe9e511cafd37e68f44e2014f07ba68d27
Zheng Chen <zheng.chen@no-trade-no.life>
2024-08-30 22:22:48 +0800

    feat: new crypto hosts (#778)

---
+++ apps/hosts/src/extension.ts
@@ -0,0 +1,190 @@
+import { IExtensionContext, makeDockerEnvs, makeK8sEnvs } from '@yuants/extension';
+export default (context: IExtensionContext) => {
+  const COMPONENT_NAME = 'app-hosts';
+  context.registerDeployProvider({
+    make_json_schema: () => ({
+      type: 'object',
+      title: 'Hosts',
+      properties: {
+        env: {
+          type: 'object',
+          required: ['ADMIN_SECRET_KEY'],
+          properties: {
+            ADMIN_SECRET_KEY: { type: 'string' },
+          },
+        },
+        network: {
+          type: 'object',
+          properties: {
+            backward_proxy: {
+              type: 'object',
+              properties: {
+                host: { type: 'string' },
+              },
+            },
+            port_forward: {
+              type: 'object',
+              properties: {
+                host: { type: 'number' },
+              },
+            },
+          },
+        },
+      },
+    }),
+    make_docker_compose_file: async (ctx, envCtx) => ({
+      host: {
+        image: `ghcr.io/no-trade-no-life/${COMPONENT_NAME}:${ctx.version ?? envCtx.version}`,
+        ports: [['host', 8888]]
+          .filter(([name]) => ctx.network?.port_forward?.[name] !== undefined)
+          .map(([name, targetPort]) => `${ctx.network!.port_forward![name]}:${targetPort}`),
+        environment: makeDockerEnvs(ctx.env),
+      },
+    }),
+    make_k8s_resource_objects: async (ctx, envCtx) => ({
+      deployment: {
+        apiVersion: 'apps/v1',
+        kind: 'Deployment',
+        metadata: {
+          name: COMPONENT_NAME,
+          namespace: 'yuan',
+          labels: {
+            'y.ntnl.io/version': ctx.version ?? envCtx.version,
+            'y.ntnl.io/component': COMPONENT_NAME,
+          },
+        },
+        spec: {
+          replicas: 1,
+          selector: {
+            matchLabels: {
+              'y.ntnl.io/component': COMPONENT_NAME,
+            },
+          },
+          template: {
+            metadata: {
+              annotations: {},
+              labels: {
+                'y.ntnl.io/version': ctx.version ?? envCtx.version,
+                'y.ntnl.io/component': COMPONENT_NAME,
+              },
+            },
+            spec: {
+              imagePullSecrets: [
+                {
+                  name: 'pull-secret', // TODO(wsy): such things should be managed at namespace scope
+                },
+              ],
+              containers: [
+                {
+                  name: COMPONENT_NAME,
+                  image: `ghcr.io/no-trade-no-life/${COMPONENT_NAME}:${ctx.version ?? envCtx.version}`,
+                  imagePullPolicy: 'IfNotPresent',
+                  env: makeK8sEnvs(ctx.env),
+                  ports: [
+                    {
+                      name: 'host',
+                      containerPort: 8888,
+                      protocol: 'TCP',
+                    },
+                  ],
+                  resources: {
+                    limits: {
+                      cpu: ctx.cpu?.max ?? '1000m',
+                      memory: ctx.memory?.max ?? '1024Mi',
+                    },
+                    requests: {
+                      cpu: ctx.cpu?.min ?? '100m',
+                      memory: ctx.memory?.min ?? '128Mi',
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      },
+      service: {
+        apiVersion: 'v1',
+        kind: 'Service',
+        metadata: {
+          name: COMPONENT_NAME,
+          namespace: 'yuan',
+          labels: {
+            'y.ntnl.io/version': ctx.version ?? envCtx.version,
+            'y.ntnl.io/component': COMPONENT_NAME,
+          },
+        },
+        spec: {
+          type: 'ClusterIP',
+          ports: ['host']
+            .map((name) => ({
+              port: ctx.network?.port_forward?.[name],
+              targetPort: name,
+              name,
+              protocol: 'TCP',
+            }))
+            .filter(({ port }) => port != undefined),
+          selector: {
+            'y.ntnl.io/component': COMPONENT_NAME,
+          },
+        },
+      },
+      ingress: {
+        apiVersion: 'networking.k8s.io/v1',
+        kind: 'Ingress',
+        metadata: {
+          name: COMPONENT_NAME,
+          namespace: 'yuan',
+          annotations: {
+            'cert-manager.io/cluster-issuer': 'letsencrypt-prod', // TODO(wsy): make this a dependent value
+            // 'nginx.ingress.kubernetes.io/auth-signin': `https://${
+            //   ctx.annotations!.sso_url
+            // }/oauth2/start?rd=$scheme://$best_http_host$request_uri`,
+            // 'nginx.ingress.kubernetes.io/auth-url': `https://${ctx.annotations!.sso_url}/oauth2/auth`
+          },
+          labels: {
+            'y.ntnl.io/version': ctx.version ?? envCtx.version,
+            'y.ntnl.io/component': COMPONENT_NAME,
+          },
+        },
+        spec: {
+          ingressClassName: 'nginx',
+          rules: ['host']
+            .filter(
+              (v) =>
+                ctx.network?.backward_proxy?.[v] !== undefined &&
+                ctx.network?.port_forward?.[v] !== undefined,
+            )
+            .map((name) => {
+              const parts = new URL(ctx.network!.backward_proxy![name]);
+              return {
+                host: parts.host!,
+                http: {
+                  paths: [
+                    {
+                      path: parts.pathname,
+                      pathType: 'Prefix',
+                      backend: {
+                        service: {
+                          name,
+                          port: {
+                            number: ctx.network!.port_forward![name],
+                          },
+                        },
+                      },
+                    },
+                  ],
+                },
+              };
+            }),
+          tls: [
+            {
+              hosts: Object.values(ctx.network!.backward_proxy ?? {}).map((v) => new URL(v).host),
+              secretName: `${COMPONENT_NAME}-tls`,
+            },
+          ],
+        },
+      },
+    }),
+  });
+};
--- libraries/utils/src/crypto.ts
+++ libraries/utils/src/crypto.ts
@@ -6,23 +6,35 @@ import bs58 from 'bs58';
  *
  * - ED25519 key pair
  * - base58 encoded
+ *
  * @public
  */
-export const createKeyPair = () => {
+export const createKeyPair = (): { public_key: string; private_key: string } => {
   const { publicKey, secretKey } = sign.keyPair();
-  return { publicKey: bs58.encode(publicKey), secretKey: bs58.encode(secretKey) };
+  return { public_key: bs58.encode(publicKey), private_key: bs58.encode(secretKey) };
 };
 
 /**
- * sign a message with a secret key
+ * create a key pair from a secret key
+ * @param privateKey - the private key to create the key pair from (base58 encoded)
+ * @returns the public key and the private key (both base58 encoded)
+ * @public
+ */
+export const fromPrivateKey = (privateKey: string): { public_key: string; private_key: string } => {
+  const pair = sign.keyPair.fromSecretKey(bs58.decode(privateKey));
+  return { public_key: bs58.encode(pair.publicKey), private_key: privateKey };
+};
+
+/**
+ * sign a message with a private key
  *
  * @param message - the message to sign
- * @param secretKey - the secret key to sign the message with (base58 encoded)
+ * @param privateKey - the private key to sign the message with (base58 encoded)
  * @returns the signature (base58 encoded)
  * @public
  */
-export const signMessage = (message: string, secretKey: string) => {
-  const secretKeyUint8Array = bs58.decode(secretKey);
+export const signMessage = (message: string, privateKey: string): string => {
+  const secretKeyUint8Array = bs58.decode(privateKey);
   const messageUint8Array = new TextEncoder().encode(message);
   const signedMessage = sign.detached(messageUint8Array, secretKeyUint8Array);
   return bs58.encode(signedMessage);
@@ -36,7 +48,7 @@ export const signMessage = (message: string, secretKey: string) => {
  * @returns true if the signature is valid, false otherwise, maybe throw an error if public key is invalid
  * @public
  */
-export const verifyMessage = (message: string, signature: string, publicKey: string) => {
+export const verifyMessage = (message: string, signature: string, publicKey: string): boolean => {
   const publicKeyUint8Array = bs58.decode(publicKey);
   const messageUint8Array = new TextEncoder().encode(message);
   const signatureUint8Array = bs58.decode(signature);
