51eaa47c2aeef8c8e4ff1a52508d8ff6c07bc643
CZ <zheng.chen@no-trade-no.life>
2024-09-01 01:12:55 +0800

    amend

--- ui/web/src/modules/Pages/LocalizedPageTitle.tsx
+++ ui/web/src/modules/Pages/LocalizedPageTitle.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Trans } from 'react-i18next';
 import i18n from '../Locale/i18n';
 
-export const LocalizePageTitle = (props: { type: string; params?: any }) => {
+export const LocalizePageTitle = (props: { type: string; params?: any }): React.ReactNode => {
   const i18nKey = `pages:${props.type}`;
   if (i18n.exists(i18nKey)) {
     return (
