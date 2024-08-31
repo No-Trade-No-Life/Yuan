7232de786f551d8debad211c9a476b0516470f87
CZ <zheng.chen@no-trade-no.life>
2024-09-01 01:30:21 +0800

    amend

--- ui/web/src/modules/Pages/LocalizedPageTitle.tsx
+++ ui/web/src/modules/Pages/LocalizedPageTitle.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Trans } from 'react-i18next';
 import i18n from '../Locale/i18n';
 
-export const LocalizePageTitle = (props: { type: string; params?: any }): React.ReactNode => {
+export const LocalizePageTitle: React.ComponentType<{ type: string; params?: any }> = (props) => {
   const i18nKey = `pages:${props.type}`;
   if (i18n.exists(i18nKey)) {
     return (
       <Trans i18nKey={i18nKey} values={props.params} tOptions={{ interpolation: { escapeValue: false } }} />
     );
-  } else {
-    return props.type;
   }
+  return props.type;
 };
