7bea7d47e46e81ddbea7f470235ec73ddefcd535
Zheng Chen <zheng.chen@no-trade-no.life>
2024-09-02 07:58:09 +0800

    feat(gui): add command to reload wallpaper folder (#792)

--- ui/web/src/modules/DesktopLayout/WallPaper.tsx
+++ ui/web/src/modules/DesktopLayout/WallPaper.tsx
@@ -1,11 +1,12 @@
 import { useObservable, useObservableState } from 'observable-hooks';
 import { extname, join } from 'path-browserify';
 import React from 'react';
-import { filter, from, map, mergeMap, toArray } from 'rxjs';
-import { FsBackend$, fs } from '../FileSystem';
+import { Subject, filter, from, map, merge, mergeMap, toArray } from 'rxjs';
 import { createPersistBehaviorSubject } from '../BIOS';
 import { registerCommand } from '../CommandCenter';
+import { FsBackend$, fs } from '../FileSystem';
 
+const reloadWallPaper$ = new Subject<void>();
 const currentWallPaperIndex$ = createPersistBehaviorSubject('wall-paper-index', 0);
 
 registerCommand('WallPaper.next', () => {
@@ -15,53 +16,59 @@ registerCommand('WallPaper.prev', () => {
   currentWallPaperIndex$.next((currentWallPaperIndex$.value || 0) - 1);
 });
 
+registerCommand('WallPaper.reload', () => {
+  reloadWallPaper$.next();
+});
+
 export const WallPaper = React.memo(() => {
   // Load WallPaper from Workspace
   const wallPaperURLs = useObservableState(
     useObservable(() =>
-      FsBackend$.pipe(
-        mergeMap(async () => {
-          const wallpaper_dir = '/.Y/wallpapers';
-          const wallpapers = await fs.readdir(wallpaper_dir);
-          return wallpapers.map((x) => join(wallpaper_dir, x));
-        }),
-      ).pipe(
-        //
-        map((x) => x.sort((a, b) => a.localeCompare(b))),
-        mergeMap((x) =>
-          from(x).pipe(
-            map((filename) => {
-              const ext = extname(filename);
-              // TODO: add more MIME types mapping here
-              const mime = {
-                '.gif': 'image/gif',
-                '.png': 'image/png',
-                '.jpg': 'image/jpeg',
-                '.jpeg': 'image/jpeg',
-                '.mp4': 'video/mp4',
-                '.webm': 'video/webm',
-                '.ogv': 'video/ogg',
-                '.mpeg': 'video/mpeg',
-                '.mov': 'video/quicktime',
-                '.avi': 'video/x-msvideo',
-                '.3gp': 'video/3gpp',
-                '.3g2': 'video/3gpp2',
-              }[ext];
-              if (!mime) return null;
-              return { filename, mime };
-            }),
-            filter((x): x is Exclude<typeof x, null> => !!x),
-            mergeMap(({ filename, mime }) =>
-              // Blob supports big file (about 100MB tested)
-              from(fs.readFileAsBlob(filename)).pipe(
-                map((blob) => URL.createObjectURL(blob)),
-                map((url) => ({ filename, url, mime })),
+      merge(FsBackend$, reloadWallPaper$)
+        .pipe(
+          mergeMap(async () => {
+            const wallpaper_dir = '/.Y/wallpapers';
+            const wallpapers = await fs.readdir(wallpaper_dir);
+            return wallpapers.map((x) => join(wallpaper_dir, x));
+          }),
+        )
+        .pipe(
+          //
+          map((x) => x.sort((a, b) => a.localeCompare(b))),
+          mergeMap((x) =>
+            from(x).pipe(
+              map((filename) => {
+                const ext = extname(filename);
+                // TODO: add more MIME types mapping here
+                const mime = {
+                  '.gif': 'image/gif',
+                  '.png': 'image/png',
+                  '.jpg': 'image/jpeg',
+                  '.jpeg': 'image/jpeg',
+                  '.mp4': 'video/mp4',
+                  '.webm': 'video/webm',
+                  '.ogv': 'video/ogg',
+                  '.mpeg': 'video/mpeg',
+                  '.mov': 'video/quicktime',
+                  '.avi': 'video/x-msvideo',
+                  '.3gp': 'video/3gpp',
+                  '.3g2': 'video/3gpp2',
+                }[ext];
+                if (!mime) return null;
+                return { filename, mime };
+              }),
+              filter((x): x is Exclude<typeof x, null> => !!x),
+              mergeMap(({ filename, mime }) =>
+                // Blob supports big file (about 100MB tested)
+                from(fs.readFileAsBlob(filename)).pipe(
+                  map((blob) => URL.createObjectURL(blob)),
+                  map((url) => ({ filename, url, mime })),
+                ),
               ),
+              toArray(),
             ),
-            toArray(),
           ),
         ),
-      ),
     ),
     [],
   );
