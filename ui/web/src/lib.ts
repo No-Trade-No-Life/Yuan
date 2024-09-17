b375ebbe0261440a08e8a468c1d7a4f5f6a00b9d
Zheng Chen <zheng.chen@no-trade-no.life>
2024-09-18 03:03:30 +0800

    feat(ui): add echarts-for-react lib (#842)

--- ui/web/src/lib.ts
+++ ui/web/src/lib.ts
@@ -3,10 +3,11 @@ import * as IconParksIcons from '@icon-park/react';
 import * as dataModel from '@yuants/data-model';
 import * as protocol from '@yuants/protocol';
 import * as yuantsUtils from '@yuants/utils';
+import * as echartsForReact from 'echarts-for-react';
 import * as observableHooks from 'observable-hooks';
-import * as yalps from 'yalps';
 import React from 'react';
 import * as rxjs from 'rxjs';
+import * as yalps from 'yalps';
 
 export const libs = {
   '@yuants/data-model': dataModel,
@@ -18,6 +19,7 @@ export const libs = {
   '@douyinfe/semi-ui': SemiUI,
   '@icon-park/react': IconParksIcons,
   'observable-hooks': observableHooks,
+  'echarts-for-react': echartsForReact,
 };
 
 Object.assign(globalThis, { Libs: libs });
