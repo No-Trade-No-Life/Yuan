284241583ef40440f6693bf68f2af273c3b97b9b
CZ <zheng.chen@no-trade-no.life>
2024-10-20 03:41:50 +0800

    feat(gui): Allow hide navigator fix(gui): init showHome doesn't work sometimes

--- ui/web/src/modules/DesktopLayout/DesktopLayout.tsx
+++ ui/web/src/modules/DesktopLayout/DesktopLayout.tsx
@@ -3,6 +3,7 @@ import { Layout, Space } from '@douyinfe/semi-ui';
 import { Actions, Layout as FlexLayout, TabNode } from 'flexlayout-react';
 import { useObservableState } from 'observable-hooks';
 import { useEffect, useState } from 'react';
+import { createPersistBehaviorSubject } from '../BIOS';
 import { CommandCenter } from '../CommandCenter';
 import { fs } from '../FileSystem';
 import { Button } from '../Interactive';
@@ -15,6 +16,8 @@ import { DarkmodeSwitch } from '../Workbench/DarkmodeSwitch';
 import { registerAssociationRule } from '../Workspace';
 import { WallPaper } from './WallPaper';
 import { layoutModel$, layoutModelJson$ } from './layout-model';
+
+export const isHideNavigator$ = createPersistBehaviorSubject('hide-navigator', false);
 
 registerAssociationRule({
   id: 'Layout',
@@ -31,6 +34,7 @@ registerAssociationRule({
 export const DesktopLayout = () => {
   const model = useObservableState(layoutModel$);
   const isShowHome = useObservableState(isShowHome$);
+  const isHideNavigator = useObservableState(isHideNavigator$);
 
   const factory = (node: TabNode) => {
     const id = node.getId();
@@ -72,7 +76,7 @@ export const DesktopLayout = () => {
       }}
     >
       <WallPaper />
-      {isShowHome ? <HomePage /> : null}
+      {isShowHome === true ? <HomePage /> : null}
       <Layout.Content style={{ position: 'relative' }}>
         {model && (
           <FlexLayout
@@ -83,10 +87,6 @@ export const DesktopLayout = () => {
               renderValues.content = (
                 <LocalizePageTitle type={node.getComponent()!} params={node.getConfig()} />
               );
-            }}
-            onTabSetPlaceHolder={() => {
-              isShowHome$.next(true);
-              return null;
             }}
             onAuxMouseClick={(node, e) => {
               if (
@@ -106,65 +106,67 @@ export const DesktopLayout = () => {
 
         <style>{style}</style>
       </Layout.Content>
-      <Layout.Header style={{ padding: 4 }}>
-        <Space style={{ width: '100%', justifyContent: 'space-between' }}>
-          <Space>
-            <ErrorBoundary>
-              <UserMenu />
-            </ErrorBoundary>
-            <Button
-              icon={<IconHome />}
-              theme="borderless"
-              type={isShowHome ? 'primary' : 'tertiary'}
-              onClick={async () => {
-                toggleShowHome();
-              }}
-            />
-          </Space>
-          <Space>
-            <CommandCenter />
-          </Space>
-          <Space>
+      {isHideNavigator === false && (
+        <Layout.Header style={{ padding: 4 }}>
+          <Space style={{ width: '100%', justifyContent: 'space-between' }}>
             <Space>
               <ErrorBoundary>
-                <NetworkStatusWidget />
+                <UserMenu />
               </ErrorBoundary>
-              <ErrorBoundary>
-                <DarkmodeSwitch />
-              </ErrorBoundary>
-              <ErrorBoundary>
-                <Button
-                  theme="borderless"
-                  type="tertiary"
-                  icon={<IconFullScreenStroked />}
-                  onClick={async () => {
-                    if (document.fullscreenElement) {
-                      return document.exitFullscreen();
-                    }
-                    function enterFullscreen(element: any) {
-                      if (element.requestFullscreen) {
-                        return element.requestFullscreen();
-                      } else if (element.mozRequestFullScreen) {
-                        // Firefox
-                        return element.mozRequestFullScreen();
-                      } else if (element.webkitRequestFullscreen) {
-                        // Chrome, Safari and Opera
-                        return element.webkitRequestFullscreen();
-                      } else if (element.msRequestFullscreen) {
-                        // IE/Edge
-                        return element.msRequestFullscreen();
+              <Button
+                icon={<IconHome />}
+                theme="borderless"
+                type={isShowHome ? 'primary' : 'tertiary'}
+                onClick={async () => {
+                  toggleShowHome();
+                }}
+              />
+            </Space>
+            <Space>
+              <CommandCenter />
+            </Space>
+            <Space>
+              <Space>
+                <ErrorBoundary>
+                  <NetworkStatusWidget />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                  <DarkmodeSwitch />
+                </ErrorBoundary>
+                <ErrorBoundary>
+                  <Button
+                    theme="borderless"
+                    type="tertiary"
+                    icon={<IconFullScreenStroked />}
+                    onClick={async () => {
+                      if (document.fullscreenElement) {
+                        return document.exitFullscreen();
                       }
-                    }
-                    return enterFullscreen(document.body);
+                      function enterFullscreen(element: any) {
+                        if (element.requestFullscreen) {
+                          return element.requestFullscreen();
+                        } else if (element.mozRequestFullScreen) {
+                          // Firefox
+                          return element.mozRequestFullScreen();
+                        } else if (element.webkitRequestFullscreen) {
+                          // Chrome, Safari and Opera
+                          return element.webkitRequestFullscreen();
+                        } else if (element.msRequestFullscreen) {
+                          // IE/Edge
+                          return element.msRequestFullscreen();
+                        }
+                      }
+                      return enterFullscreen(document.body);
 
-                    // return document.body.requestFullscreen();
-                  }}
-                ></Button>
-              </ErrorBoundary>
+                      // return document.body.requestFullscreen();
+                    }}
+                  ></Button>
+                </ErrorBoundary>
+              </Space>
             </Space>
           </Space>
-        </Space>
-      </Layout.Header>
+        </Layout.Header>
+      )}
     </Layout>
   );
 };
