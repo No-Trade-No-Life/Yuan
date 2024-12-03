574e0f33bc1b42ff7c074569e948dca05d5a2923
CZ <cz@ntnl.io>
2024-12-04 02:20:50 +0800

    feat(portal): Load Balance

--- apps/portal/src/index.ts
+++ apps/portal/src/index.ts
@@ -1,7 +1,21 @@
 import { formatTime, UUID } from '@yuants/data-model';
 import { readDataRecords, Terminal } from '@yuants/protocol';
 import { listWatch } from '@yuants/utils';
-import { defer, from, groupBy, map, mergeMap, Observable, repeat, retry, tap, toArray } from 'rxjs';
+import {
+  combineLatest,
+  defer,
+  filter,
+  from,
+  groupBy,
+  map,
+  mergeMap,
+  Observable,
+  repeat,
+  retry,
+  shareReplay,
+  tap,
+  toArray,
+} from 'rxjs';
 
 const internalTerminal = new Terminal(process.env.HOST_URL!, {
   terminal_id: `Portal/Internal/${UUID()}`,
@@ -22,31 +36,67 @@ interface IPortalRelation {
   schema: any;
 }
 
-defer(() =>
+const portalTerminals$ = from(internalTerminal.terminalInfos$).pipe(
+  mergeMap((x) =>
+    from(x).pipe(
+      //
+      filter((xx) => xx.name === internalTerminal.terminalInfo.name),
+      map((xx) => xx.terminal_id),
+      toArray(),
+      filter((x) => x.length > 0 && x.some((xx) => xx === internalTerminal.terminal_id)), // ISSUE: ignore setup-phrase no self
+    ),
+  ),
+  shareReplay(1),
+  tap((arr) => console.info(formatTime(Date.now()), 'FindPortals', arr.length)),
+);
+
+const portalRelation$ = defer(() =>
   readDataRecords(internalTerminal, {
     type: 'portal_relation',
+    options: {
+      sort: [['updated_at', 1]],
+    },
   }),
-)
-  .pipe(
-    //
-    mergeMap((x) =>
-      from(x).pipe(
-        groupBy((x) => x.origin.external_host_url),
-        mergeMap((group) =>
-          group.pipe(
-            //
-            map((v) => v.origin),
-            toArray(),
-            map((v) => ({ host_url: group.key, items: v })),
-          ),
+).pipe(
+  //
+  retry({ delay: 5000 }),
+  repeat({ delay: 30_000 }),
+  shareReplay(1),
+  tap((arr) => console.info(formatTime(Date.now()), 'FetchedPortalRelations', arr.length)),
+);
+
+const portalRelationsGroupByExternalHost$ = portalRelation$.pipe(
+  mergeMap((x) =>
+    from(x).pipe(
+      groupBy((x) => x.origin.external_host_url),
+      mergeMap((group) =>
+        group.pipe(
+          //
+          map((v) => v.origin),
+          toArray(),
+          map((v) => ({ host_url: group.key, items: v })),
         ),
-        toArray(),
       ),
+      toArray(),
     ),
-    retry({ delay: 5000 }),
-    repeat({ delay: 30_000 }),
+  ),
+);
+
+combineLatest([portalRelationsGroupByExternalHost$, portalTerminals$])
+  .pipe(
+    // Load balance: many portals in one host, sharing workload
+    map(([list, portals]) => {
+      const idx = portals.indexOf(internalTerminal.terminal_id);
+      const N = list.length;
+      const M = portals.length;
+      const size = Math.floor(N / M) + (N % M > idx ? 1 : 0);
+      const startIdx = Math.floor(N / M) * idx + Math.min(idx, N % M);
+      const endIdx = startIdx + size;
+      return list.slice(startIdx, endIdx);
+    }),
   )
   .pipe(
+    tap((arr) => console.info(formatTime(Date.now()), 'OwnsHostsCount', arr.length)),
     listWatch(
       (x) => x.host_url,
       (x) => {
