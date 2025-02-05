2e073a1858aa8baf542d6554f0c2ef3ff17c84a6
zccz14 <zheng.chen@no-trade-no.life>
2025-02-05 22:14:59 +0800

    refactor(AccountList): simplify account info retrieval and update DataView structure

--- ui/web/src/modules/AccountInfo/AccountList.tsx
+++ ui/web/src/modules/AccountInfo/AccountList.tsx
@@ -1,122 +1,98 @@
-import { Space, Spin, Typography } from '@douyinfe/semi-ui';
-import { createColumnHelper } from '@tanstack/react-table';
+import { Button, Typography } from '@douyinfe/semi-ui';
 import { formatTime } from '@yuants/data-model';
-import { useObservableState } from 'observable-hooks';
-import { useMemo } from 'react';
-import { executeCommand } from '../CommandCenter';
-import { Button, DataView } from '../Interactive';
+import { useObservable, useObservableState } from 'observable-hooks';
+import { combineLatest, concatWith, from, map, mergeMap, of, switchMap, toArray } from 'rxjs';
+import { DataView } from '../Interactive';
 import { registerPage } from '../Pages';
 import { accountIds$, useAccountInfo } from './model';
+import { executeCommand } from '../CommandCenter';
 
 registerPage('AccountList', () => {
-  const accountIds = useObservableState(accountIds$, []);
+  const accountInfos = useObservableState(
+    useObservable(
+      () =>
+        accountIds$.pipe(
+          switchMap((accountIds) =>
+            from(accountIds).pipe(
+              //
+              map((accountId) =>
+                of(undefined).pipe(
+                  concatWith(useAccountInfo(accountId)),
+                  map((info) => ({ accountId, info })),
+                ),
+              ),
+              toArray(),
+              mergeMap((x$) => combineLatest(x$)),
+            ),
+          ),
+        ),
+      [],
+    ),
+    [],
+  );
 
-  const columns = useMemo(() => {
-    const columnHelper = createColumnHelper<string>();
-    return [
-      columnHelper.accessor((x) => x, {
-        id: 'account_id',
-        header: () => '账户 ID',
-        cell: (x) => <Typography.Text copyable>{x.renderValue()}</Typography.Text>,
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'currency',
-        header: () => '货币',
-        cell: (x) => {
-          const account_id = x.row.original;
-          const accountInfo$ = useMemo(() => useAccountInfo(account_id), [account_id]);
-          const accountInfo = useObservableState(accountInfo$);
-
-          return accountInfo?.money.currency;
+  return (
+    <DataView
+      data={accountInfos}
+      columns={[
+        {
+          accessorKey: 'accountId',
+          header: () => '账户 ID',
+          cell: (x) => <Typography.Text copyable>{x.renderValue()}</Typography.Text>,
         },
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'equity',
-        header: () => '净值',
-        cell: (x) => {
-          const account_id = x.row.original;
-          const accountInfo$ = useMemo(() => useAccountInfo(account_id), [account_id]);
-          const accountInfo = useObservableState(accountInfo$);
-
-          return accountInfo?.money.equity;
+        {
+          accessorKey: 'info.money.currency',
+          header: () => '货币',
         },
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'balance',
-        header: () => '余额',
-        cell: (x) => {
-          const account_id = x.row.original;
-          const accountInfo$ = useMemo(() => useAccountInfo(account_id), [account_id]);
-          const accountInfo = useObservableState(accountInfo$);
-
-          return accountInfo?.money.balance;
+        {
+          accessorKey: 'info.money.equity',
+          header: () => '净值',
         },
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'profit',
-        header: () => '盈亏',
-        cell: (x) => {
-          const account_id = x.row.original;
-          const accountInfo$ = useMemo(() => useAccountInfo(account_id), [account_id]);
-          const accountInfo = useObservableState(accountInfo$);
-
-          return accountInfo?.money.profit;
+        {
+          accessorKey: 'info.money.balance',
+          header: () => '余额',
         },
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'used-margin-ratio',
-        header: () => '保证金使用率',
-        cell: (x) => {
-          const account_id = x.row.original;
-          const accountInfo$ = useMemo(() => useAccountInfo(account_id), [account_id]);
-          const accountInfo = useObservableState(accountInfo$);
-
-          if (!accountInfo) return null;
-
-          const value = (accountInfo.money.used / accountInfo.money.equity) * 100;
-          if (Number.isNaN(value)) return 'N/A';
-
-          return value.toFixed(2) + '%';
+        {
+          accessorKey: 'info.money.profit',
+          header: () => '盈亏',
         },
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'time',
-        header: () => '更新时间',
-        cell: (x) => {
-          const account_id = x.row.original;
-          const accountInfo$ = useMemo(() => useAccountInfo(account_id), [account_id]);
-          const accountInfo = useObservableState(accountInfo$);
-
-          if (!accountInfo) return <Spin />;
-
-          const updated_at = accountInfo.updated_at || 0;
-          const timeLag = Date.now() - updated_at;
-
-          return (
-            <Space>
-              {formatTime(updated_at)}
-              {timeLag > 60_000 && (
-                <Typography.Text type="warning">
-                  信息更新于 {formatTime(updated_at)}，已经 {(timeLag / 1000).toFixed(0)}{' '}
-                  秒未更新，可能已经失去响应
-                </Typography.Text>
-              )}
-            </Space>
-          );
+        {
+          accessorKey: 'info.money.free',
+          header: () => '可用保证金',
         },
-      }),
-      columnHelper.accessor((x) => x, {
-        id: 'actions',
-        header: () => '操作',
-        cell: (x) => {
-          const account_id = x.row.original;
-          return <Button onClick={() => executeCommand('AccountInfoPanel', { account_id })}>详情</Button>;
+        {
+          id: 'used-margin-ratio',
+          accessorFn: (x) => {
+            const accountInfo = x.info;
+            if (!accountInfo) return NaN;
+            return accountInfo.money.used / accountInfo.money.equity;
+          },
+          header: () => '保证金使用率',
+          cell: (x) => {
+            const v = x.getValue();
+            if (isNaN(v)) return 'N/A';
+            return `${(x.getValue() * 100).toFixed(2)}%`;
+          },
         },
-      }),
-    ];
-  }, []);
+        {
+          accessorKey: 'info.updated_at',
+          header: () => '更新时间',
+          cell: (x) => formatTime(x.getValue()),
+        },
 
-  const data = accountIds;
-
-  return <DataView data={data} columns={columns} />;
+        {
+          header: '操作',
+          cell: (x) => {
+            return (
+              <Button
+                onClick={() => executeCommand('AccountInfoPanel', { account_id: x.row.original.accountId })}
+              >
+                查看
+              </Button>
+            );
+          },
+        },
+      ]}
+    />
+  );
 });
