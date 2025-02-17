95200220fc5028fe8f6945117d8b5a2657df7930
Siyuan Wang <c.one@thrimbda.com>
2025-02-18 02:44:20 +0800

    feat(pg): create hypertable for time series data (#1068)

--- apps/telegram-monitor/src/migration.ts
+++ apps/telegram-monitor/src/migration.ts
@@ -39,4 +39,42 @@ AddMigration({
 	`,
 });
 
+AddMigration({
+  id: '9f5e4d03-2e50-4977-9168-2d1b616f8b04',
+  name: 'create-hypertable-for-telegram_messages',
+  dependencies: ['eb62f3a4-11fd-4386-a86d-1841c3beee13'],
+  statement: `
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+ALTER TABLE telegram_messages DROP CONSTRAINT IF EXISTS telegram_messages_pkey;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conrelid = 'telegram_messages'::regclass
+        AND conname = 'telegram_messages_key'
+    ) THEN
+        ALTER TABLE telegram_messages
+        ADD CONSTRAINT telegram_messages_key
+        UNIQUE (id, created_at);
+    END IF;
+END $$;
+ALTER TABLE telegram_messages DROP CONSTRAINT IF EXISTS telegram_messages_message_id_key;
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conrelid = 'telegram_messages'::regclass
+        AND conname = 'telegram_messages_message_id_created_at_key'
+    ) THEN
+        ALTER TABLE telegram_messages
+        ADD CONSTRAINT telegram_messages_message_id_created_at_key
+        UNIQUE (message_id, created_at);
+    END IF;
+END $$;
+SELECT create_hypertable('telegram_messages', by_range('created_at'), migrate_data => TRUE, if_not_exists => TRUE);
+`,
+});
+
 ExecuteMigrations(terminal);
