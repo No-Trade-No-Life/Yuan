7190a592cc9ebd8d17613ce191d9113f9c6f3fb3
Zheng Chen <zheng.chen@no-trade-no.life>
2025-02-20 23:58:31 +0800

    feat(ui): add SQL Console module and integrate with Monaco Editor (#1082)

--- ui/web/src/modules/Interactive/TableView.tsx
+++ ui/web/src/modules/Interactive/TableView.tsx
@@ -18,117 +18,119 @@ export function TableView<T>(props: { table: Table<T>; topSlot?: React.ReactNode
   return (
     <Space vertical align="start" style={{ width: '100%' }}>
       <Space wrap>{props.topSlot}</Space>
-      <table className="semi-table">
-        <thead
-          className="semi-table-thead"
-          style={{ position: 'sticky', zIndex: 1, top: 0, background: 'var(--semi-color-bg-1)' }}
-        >
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id} className="semi-table-row">
-              {headerGroup.headers.map((header) => (
-                <th key={header.id} className="semi-table-row-head">
-                  <Space vertical>
-                    <Space>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(header.column.columnDef.header, header.getContext())}
+      <Space style={{ width: '100%', overflowX: 'auto' }}>
+        <table className="semi-table" style={{ whiteSpace: 'nowrap' }}>
+          <thead
+            className="semi-table-thead"
+            style={{ position: 'sticky', zIndex: 1, top: 0, background: 'var(--semi-color-bg-1)' }}
+          >
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id} className="semi-table-row">
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} className="semi-table-row-head">
+                    <Space vertical>
+                      <Space>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(header.column.columnDef.header, header.getContext())}
 
-                      {header.column.getCanSort() ? (
-                        <Button
-                          theme="borderless"
-                          icon={
-                            {
-                              asc: <IconCaretup />,
-                              desc: <IconCaretdown />,
-                              false: <IconSort />,
-                            }[header.column.getIsSorted() as string] ?? null
+                        {header.column.getCanSort() ? (
+                          <Button
+                            theme="borderless"
+                            icon={
+                              {
+                                asc: <IconCaretup />,
+                                desc: <IconCaretdown />,
+                                false: <IconSort />,
+                              }[header.column.getIsSorted() as string] ?? null
+                            }
+                            onClick={header.column.getToggleSortingHandler()}
+                          />
+                        ) : null}
+
+                        <Dropdown
+                          content={
+                            <DropdownMenu>
+                              <DropdownItem
+                                icon={<IconEyeOpened />}
+                                onClick={header.column.getToggleVisibilityHandler()}
+                              >
+                                隐藏字段
+                              </DropdownItem>
+                              {header.column.getCanGroup() && (
+                                <DropdownItem
+                                  icon={header.column.getIsGrouped() ? <IconArrowRight /> : <IconArrowLeft />}
+                                  onClick={header.column.getToggleGroupingHandler()}
+                                >
+                                  {header.column.getIsGrouped() ? '取消分组' : '分组'}
+                                </DropdownItem>
+                              )}
+                            </DropdownMenu>
                           }
-                          onClick={header.column.getToggleSortingHandler()}
+                        >
+                          <IconMenu />
+                        </Dropdown>
+                      </Space>
+
+                      {header.column.getCanFilter() ? (
+                        <Input
+                          value={header.column.getFilterValue() as string}
+                          placeholder={'搜索...'}
+                          onChange={(v) => {
+                            header.column.setFilterValue(v);
+                          }}
                         />
                       ) : null}
-
-                      <Dropdown
-                        content={
-                          <DropdownMenu>
-                            <DropdownItem
-                              icon={<IconEyeOpened />}
-                              onClick={header.column.getToggleVisibilityHandler()}
-                            >
-                              隐藏字段
-                            </DropdownItem>
-                            {header.column.getCanGroup() && (
-                              <DropdownItem
-                                icon={header.column.getIsGrouped() ? <IconArrowRight /> : <IconArrowLeft />}
-                                onClick={header.column.getToggleGroupingHandler()}
-                              >
-                                {header.column.getIsGrouped() ? '取消分组' : '分组'}
-                              </DropdownItem>
-                            )}
-                          </DropdownMenu>
-                        }
-                      >
-                        <IconMenu />
-                      </Dropdown>
                     </Space>
-
-                    {header.column.getCanFilter() ? (
-                      <Input
-                        value={header.column.getFilterValue() as string}
-                        placeholder={'搜索...'}
-                        onChange={(v) => {
-                          header.column.setFilterValue(v);
-                        }}
-                      />
-                    ) : null}
-                  </Space>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody className="semi-table-tbody">
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id} className="semi-table-row">
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <td key={cell.id} className="semi-table-row-cell">
-                    {cell.getIsGrouped() ? (
-                      <Space>
-                        <Button
-                          theme="borderless"
-                          icon={row.getIsExpanded() ? <IconTreeTriangleDown /> : <IconTreeTriangleRight />}
-                          onClick={row.getToggleExpandedHandler()}
-                        />
-                        {flexRender(cell.column.columnDef.cell, cell.getContext())} ({row.subRows.length})
-                      </Space>
-                    ) : cell.getIsAggregated() ? (
-                      flexRender(
-                        cell.column.columnDef.aggregatedCell ?? cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )
-                    ) : cell.getIsPlaceholder() ? null : (
-                      flexRender(cell.column.columnDef.cell, cell.getContext())
-                    )}
-                  </td>
-                );
-              })}
-            </tr>
-          ))}
-        </tbody>
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(header.column.columnDef.footer, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
-      </table>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="semi-table-tbody">
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className="semi-table-row">
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id} className="semi-table-row-cell">
+                      {cell.getIsGrouped() ? (
+                        <Space>
+                          <Button
+                            theme="borderless"
+                            icon={row.getIsExpanded() ? <IconTreeTriangleDown /> : <IconTreeTriangleRight />}
+                            onClick={row.getToggleExpandedHandler()}
+                          />
+                          {flexRender(cell.column.columnDef.cell, cell.getContext())} ({row.subRows.length})
+                        </Space>
+                      ) : cell.getIsAggregated() ? (
+                        flexRender(
+                          cell.column.columnDef.aggregatedCell ?? cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )
+                      ) : cell.getIsPlaceholder() ? null : (
+                        flexRender(cell.column.columnDef.cell, cell.getContext())
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            {table.getFooterGroups().map((footerGroup) => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.footer, header.getContext())}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tfoot>
+        </table>
+      </Space>
     </Space>
   );
 }
---
+++ ui/web/src/modules/SQL/Console.tsx
@@ -0,0 +1,68 @@
+import { Space } from '@douyinfe/semi-ui';
+import { ColumnDef } from '@tanstack/react-table';
+import { UUID } from '@yuants/data-model';
+import '@yuants/sql';
+import { editor } from 'monaco-editor';
+import { useMemo, useRef, useState } from 'react';
+import { firstValueFrom } from 'rxjs';
+import { executeCommand, registerCommand } from '../CommandCenter';
+import { MonacoEditor } from '../Editor/Monaco';
+import { Button, DataView, Toast } from '../Interactive';
+import { registerPage, usePageParams } from '../Pages';
+import { terminal$ } from '../Terminals';
+
+registerCommand('SQL/Console/New', () => executeCommand('SQLConsole', { id: UUID() }));
+
+registerPage('SQLConsole', () => {
+  const { id } = usePageParams() as { id: string };
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const [data, setData] = useState([] as any[]);
+  const columns = useMemo(() => {
+    const a = data[0] || {};
+    return Object.entries(a).map(([key, value]): ColumnDef<any, any> => ({ header: key, accessorKey: key }));
+  }, [data]);
+  return (
+    <Space vertical align="start" style={{ width: '100%', height: '100%' }}>
+      <Space>
+        <Button
+          onClick={async () => {
+            try {
+              const terminal = await firstValueFrom(terminal$);
+              if (!terminal) throw 'Terminal not found';
+              const query = editorRef.current?.getValue();
+              if (!query) throw 'Empty query';
+              const t = Date.now();
+              const res = await terminal.requestForResponse('SQL', { query });
+              if (res.code === 0 && res.data) {
+                setData(res.data as any[]);
+              } else {
+                throw res.message;
+              }
+              editorRef.current?.setValue(
+                editorRef.current.getValue() + '\n' + `/* DONE in ${Date.now() - t} ms */`,
+              );
+            } catch (e) {
+              editorRef.current?.setValue(
+                editorRef.current.getValue() + '\n' + '/* ERROR: ' + `${e}` + ' */',
+              );
+            }
+          }}
+        >
+          执行
+        </Button>
+      </Space>
+      <div style={{ width: '100%', height: '40%' }}>
+        <MonacoEditor
+          language="sql"
+          value=""
+          onConstruct={(editor) => {
+            editorRef.current = editor;
+          }}
+        />
+      </div>
+      <div style={{ width: '100%' }}>
+        <DataView data={data} columns={columns} columnsDependencyList={[data]} />
+      </div>
+    </Space>
+  );
+});
