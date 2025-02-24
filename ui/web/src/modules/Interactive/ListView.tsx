106b4dd723b1dc37b8d533cb91d72abc5e65fee4
Zheng Chen <zheng.chen@no-trade-no.life>
2025-02-24 16:40:12 +0800

    feat(ui): refactor DataView and ListView to improve layout and remove topSlot prop (#1086)

--- ui/web/src/modules/Interactive/ListView.tsx
+++ ui/web/src/modules/Interactive/ListView.tsx
@@ -2,40 +2,35 @@ import { Descriptions, List, Space } from '@douyinfe/semi-ui';
 import { Table, flexRender } from '@tanstack/react-table';
 import React from 'react';
 
-export function ListView<T>(props: { table: Table<T>; topSlot?: React.ReactNode }) {
+export function ListView<T>(props: { table: Table<T> }) {
   const { table } = props;
   return (
-    <Space vertical align="start" style={{ width: '100%' }}>
-      <Space wrap style={{ width: '100%' }}>
-        {props.topSlot}
-      </Space>
-      <List>
-        {table.getRowModel().rows.map((row) => (
-          <List.Item key={row.id}>
-            <Descriptions>
-              {table.getHeaderGroups().map((headerGroup) =>
-                headerGroup.headers.map((header, idx) => {
-                  return (
-                    <Descriptions.Item
-                      key={headerGroup.id + '/' + idx}
-                      itemKey={
-                        header.isPlaceholder
-                          ? null
-                          : flexRender(header.column.columnDef.header, header.getContext())
-                      }
-                    >
-                      {row
-                        .getVisibleCells()
-                        .filter((cell) => cell.column.id === header.column.id)
-                        .map((cell) => flexRender(cell.column.columnDef.cell, cell.getContext()))}
-                    </Descriptions.Item>
-                  );
-                }),
-              )}
-            </Descriptions>
-          </List.Item>
-        ))}
-      </List>
-    </Space>
+    <List style={{ width: '100%' }}>
+      {table.getRowModel().rows.map((row) => (
+        <List.Item key={row.id}>
+          <Descriptions>
+            {table.getHeaderGroups().map((headerGroup) =>
+              headerGroup.headers.map((header, idx) => {
+                return (
+                  <Descriptions.Item
+                    key={headerGroup.id + '/' + idx}
+                    itemKey={
+                      header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())
+                    }
+                  >
+                    {row
+                      .getVisibleCells()
+                      .filter((cell) => cell.column.id === header.column.id)
+                      .map((cell) => flexRender(cell.column.columnDef.cell, cell.getContext()))}
+                  </Descriptions.Item>
+                );
+              }),
+            )}
+          </Descriptions>
+        </List.Item>
+      ))}
+    </List>
   );
 }
