26930fa040219cfd0a02ab4e0ddf27704c3d2532
zccz14 <zheng.chen@no-trade-no.life>
2025-02-26 16:11:26 +0800

    1

--- ui/web/src/modules/Terminals/is-connected.ts
+++ ui/web/src/modules/Terminals/is-connected.ts
@@ -1,7 +1,7 @@
-import { defer, repeat, retry, shareReplay, switchMap } from 'rxjs';
+import { EMPTY, shareReplay, switchMap } from 'rxjs';
 import { terminal$ } from './create-connection';
 
 export const isTerminalConnnected$ = terminal$.pipe(
-  switchMap((terminal) => terminal.isConnected$),
+  switchMap((terminal) => terminal?.isConnected$ ?? EMPTY),
   shareReplay(1),
 );
