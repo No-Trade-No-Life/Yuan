cc9703c32180eac69715e90689553b6d7fc8aeaf
Zheng Chen <zheng.chen@no-trade-no.life>
2025-04-14 03:23:12 +0800

    refactor(Pages): remove activePage$ BehaviorSubject from index export (#1146)

--- ui/web/src/modules/Pages/index.ts
+++ ui/web/src/modules/Pages/index.ts
@@ -1,7 +1,4 @@
-import { BehaviorSubject } from 'rxjs';
-
 export * from './ErrorBoundary';
 export * from './LocalizedPageTitle';
 export { AvailableComponents, pageRegistered$, registerPage } from './model';
 export { Page, usePageId, usePageParams, usePageTitle, usePageType, usePageViewport } from './Page';
-export const activePage$ = new BehaviorSubject<{ page: string; pageParams: any } | null>(null);
