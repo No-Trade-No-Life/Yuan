5f9edac9fe5e514cc378937d0f8f9bdce62b4ed9
Siyuan Wang <c.one@thrimbda.com>
2025-04-27 13:44:13 +0800

    feat(server): abort request when timeout (#1158)

--- apps/postgres-storage/src/index.ts
+++ apps/postgres-storage/src/index.ts
@@ -1,9 +1,10 @@
 import { formatTime, UUID } from '@yuants/data-model';
 import { Terminal } from '@yuants/protocol';
 import type {} from '@yuants/sql';
-import { Pool } from 'pg';
-import { parse } from 'pg-connection-string';
-import { first, from } from 'rxjs';
+// import { Pool } from 'pg';
+// import { parse } from 'pg-connection-string';
+import postgres from 'postgres';
+import { first, from, tap } from 'rxjs';
 
 const HOST_URL = process.env.HOST_URL!;
 const TERMINAL_ID = process.env.TERMINAL_ID || `Postgres/${UUID()}`;
@@ -13,20 +14,27 @@ const terminal = new Terminal(HOST_URL, {
   name: 'Postgres Storage',
 });
 
-const config = parse(process.env.POSTGRES_URI!);
-//@ts-ignore
-config.ssl = {
-  rejectUnauthorized: false,
-};
-
-// @ts-ignore
-const pool = new Pool({
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-  keepAlive: true,
-  ...config,
+const sql = postgres(process.env.POSTGRES_URI!, {
+  // ISSUE: automatically close the connection after 20 seconds of inactivity or 30 minutes of lifetime
+  //   otherwise, the connection will not be closed and will cause the client memory to leak
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
 });
+
+// const config = parse(process.env.POSTGRES_URI!);
+// //@ts-ignore
+// config.ssl = {
+//   rejectUnauthorized: false,
+// };
+
+// // @ts-ignore
+// const pool = new Pool({
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+//   keepAlive: true,
+//   ...config,
+// });
 
 terminal.provideService(
   'SQL',
@@ -41,25 +49,27 @@ terminal.provideService(
   },
   async (msg, { isAborted$ }) => {
     console.info(formatTime(Date.now()), 'SQL REQUEST', msg.trace_id);
-    const sql = await pool.connect();
-    try {
-      const query = sql.query(msg.req.query);
-      from(isAborted$)
-        .pipe(first((x) => x))
-        .subscribe(() => {
+    // @ts-ignore
+    const query = sql.unsafe(msg.req.query);
+
+    from(isAborted$)
+      .pipe(
+        //
+        first((x) => x),
+        tap(() => {
           console.info(formatTime(Date.now()), 'SQL ABORTED', msg.trace_id);
-          sql.release();
-          throw new Error('Aborted');
-        });
+          // ISSUE: cancel will break the sql query, which will cause the query to fail and be caught in the catch block
+          query.cancel();
+        }),
+      )
+      .subscribe();
+
+    try {
       const results = await query;
-      sql.release();
-      const rows = Array.isArray(results) ? results.map((result) => result.rows) : results.rows;
-      // @ts-ignore
-      console.info(formatTime(Date.now()), 'SQL RESPONSE', msg.trace_id, rows.length);
-      return { res: { code: 0, message: 'OK', data: rows } };
+      console.info(formatTime(Date.now()), 'SQL RESPONSE', msg.trace_id, results.length);
+      return { res: { code: 0, message: 'OK', data: results } };
     } catch (e) {
       console.error(formatTime(Date.now()), 'SQL ERROR', msg.trace_id, e);
-      sql.release();
       throw e;
     }
   },
