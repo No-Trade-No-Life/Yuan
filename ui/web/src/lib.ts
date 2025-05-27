24e9cd9fdbc0606710e2d0566453980eb993517b
zccz14 <zheng.chen@no-trade-no.life>
2025-05-27 19:48:40 +0800

    feat(lib): reorder imports and add missing dependencies for i18next and ethers

--- ui/web/src/lib.ts
+++ ui/web/src/lib.ts
@@ -1,15 +1,17 @@
-import * as SemiUI from '@douyinfe/semi-ui';
 import * as SemiUIIcon from '@douyinfe/semi-icons';
+import * as SemiUI from '@douyinfe/semi-ui';
 import * as IconParksIcons from '@icon-park/react';
 import * as dataModel from '@yuants/data-model';
 import * as protocol from '@yuants/protocol';
 import * as yuantsUtils from '@yuants/utils';
 import echartsForReact from 'echarts-for-react';
+import * as ethers from 'ethers';
+import * as i18next from 'i18next';
 import * as observableHooks from 'observable-hooks';
 import React from 'react';
+import * as reacti18next from 'react-i18next';
 import * as rxjs from 'rxjs';
 import * as yalps from 'yalps';
-import * as ethers from 'ethers';
 
 export const libs = {
   '@yuants/data-model': dataModel,
@@ -19,6 +21,8 @@ export const libs = {
   rxjs,
   yalps,
   '@douyinfe/semi-ui': SemiUI,
+  i18next,
+  'react-i18next': reacti18next,
   '@douyinfe/semi-icons': SemiUIIcon,
   '@icon-park/react': IconParksIcons,
   'observable-hooks': observableHooks,
