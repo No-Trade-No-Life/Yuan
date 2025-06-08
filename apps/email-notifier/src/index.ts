ba5c9b3a18e4e834fc312664ba827f6f592efc09
CZ <cz@ntnl.io>
2025-06-09 02:15:56 +0800

    1

--- apps/email-notifier/src/index.ts
+++ apps/email-notifier/src/index.ts
@@ -22,7 +22,7 @@ import {
 import './models/Email';
 
 const terminal = new Terminal(process.env.HOST_URL!, {
-  terminal_id: process.env.TERMINAL_ID || `Email/${process.env.EMAIL_USER}/${UUID()}`,
+  terminal_id: process.env.TERMINAL_ID || `Email/${process.env.EMAIL_USER}`,
   name: 'Email',
 });
 
