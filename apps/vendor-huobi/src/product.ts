aef468f6536e0721c26d74022a62f11e2222083c
Zheng Chen <zheng.chen@no-trade-no.life>
2025-07-13 01:18:15 +0800

    feat: integrate caching for product and leverage management (#1297)

--- apps/vendor-huobi/src/product.ts
+++ apps/vendor-huobi/src/product.ts
@@ -1,38 +1,75 @@
-import { encodePath } from '@yuants/utils';
+import { createCache } from '@yuants/cache';
 import { IProduct } from '@yuants/data-product';
 import { Terminal } from '@yuants/protocol';
 import { createSQLWriter } from '@yuants/sql';
-import { defer, filter, map, mergeMap, repeat, retry, shareReplay, Subject, tap, toArray } from 'rxjs';
+import { encodePath } from '@yuants/utils';
+import { defer, filter, map, mergeMap, repeat, retry, shareReplay, Subject, toArray } from 'rxjs';
 import { client } from './api';
 
 const product$ = new Subject<IProduct>();
 
-export const perpetualContractProducts$ = defer(() => client.getPerpetualContractSymbols()).pipe(
-  mergeMap((res) => res.data),
-  filter((symbol) => symbol.contract_status === 1),
-  map(
-    (symbol): IProduct => ({
-      datasource_id: 'HUOBI-SWAP',
-      product_id: symbol.contract_code,
-      base_currency: symbol.symbol,
-      quote_currency: 'USDT',
-      value_scale: symbol.contract_size,
-      price_step: symbol.price_tick,
-      volume_step: 1,
-      name: '',
-      value_scale_unit: '',
-      margin_rate: 0,
-      value_based_cost: 0,
-      volume_based_cost: 0,
-      max_position: 0,
-      max_volume: 0,
-      allow_long: true,
-      allow_short: true,
-      market_id: 'HUOBI/SWAP',
-    }),
-  ),
-  tap((x) => product$.next(x)),
-  toArray(),
+const cacheOfCrossLeverage = createCache({
+  readRemote: () => client.getSwapCrossLadderMargin(),
+  dispose$: Terminal.fromNodeEnv().dispose$,
+  expire: 3600_000, // 1 hour
+});
+
+const productIdToMaxLeverage = createCache({
+  readRemote: async (product_id, force_update) => {
+    const res = await cacheOfCrossLeverage.query('', force_update);
+    return res?.data
+      .find((x) => x.contract_code === product_id)
+      ?.list.reduce((acc, cur) => Math.max(acc, cur.lever_rate), 1);
+  },
+  dispose$: Terminal.fromNodeEnv().dispose$,
+  expire: 3600_000, // 1 hour
+});
+
+const swapSymbols = createCache({
+  readRemote: () => client.getPerpetualContractSymbols(),
+  expire: 3600_000,
+  dispose$: Terminal.fromNodeEnv().dispose$,
+});
+
+const swapProducts = createCache({
+  readRemote: async (_, force_update) => {
+    const products: IProduct[] = [];
+    console.info(new Date(), '正在加载合约产品信息');
+    const symbols = await swapSymbols.query('', force_update);
+    console.info(new Date(), '合约产品信息', symbols?.data);
+    for (const symbol of symbols?.data || []) {
+      if (symbol.contract_status !== 1) continue; // Only include active contracts
+      const maxLeverage = await productIdToMaxLeverage.query(symbol.contract_code, force_update);
+      const product: IProduct = {
+        datasource_id: 'HUOBI-SWAP',
+        product_id: symbol.contract_code,
+        base_currency: symbol.symbol,
+        quote_currency: 'USDT',
+        value_scale: symbol.contract_size,
+        price_step: symbol.price_tick,
+        volume_step: 1,
+        name: '',
+        value_scale_unit: '',
+        margin_rate: 1 / (maxLeverage || 1),
+        value_based_cost: 0,
+        volume_based_cost: 0,
+        max_position: 0,
+        max_volume: 0,
+        allow_long: true,
+        allow_short: true,
+        market_id: 'HUOBI/SWAP',
+      };
+      products.push(product);
+      product$.next(product);
+    }
+    return products;
+  },
+  expire: 3600_000, // 1 hour
+  dispose$: Terminal.fromNodeEnv().dispose$,
+});
+
+export const perpetualContractProducts$ = defer(() => swapProducts.query('')).pipe(
+  map((res) => res || []),
   repeat({ delay: 86400_000 }),
   retry({ delay: 10_000 }),
   shareReplay(1),
@@ -52,7 +89,7 @@ export const spotProducts$ = defer(() => client.getSpotSymbols()).pipe(
       volume_step: 1 / 10 ** symbol.tap,
       name: '',
       value_scale_unit: '',
-      margin_rate: 0,
+      margin_rate: 1,
       value_based_cost: 0,
       volume_based_cost: 0,
       max_position: 0,
