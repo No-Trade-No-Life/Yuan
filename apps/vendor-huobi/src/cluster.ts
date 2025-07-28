1ac50528d2dd855b870cdaf2d669576281f4a0a2
Siyuan Wang <c.one@thrimbda.com>
2025-07-28 18:28:11 +0800

    feat(huobi): add huobi to logger (#1318)

--- apps/vendor-huobi/src/cluster.ts
+++ apps/vendor-huobi/src/cluster.ts
@@ -2,10 +2,26 @@ import { Terminal } from '@yuants/protocol';
 import { loadSecrets } from '@yuants/secret';
 import { listWatch } from '@yuants/utils';
 import cluster from 'cluster';
-import { defer, Observable, repeat, retry } from 'rxjs';
+import { bufferTime, defer, filter, map, Observable, repeat, retry, Subject } from 'rxjs';
+import { overrideConsole } from './logger';
+
+overrideConsole();
 
 if (cluster.isPrimary) {
   console.info('This is the primary process');
+
+  const logBuffer = new Subject<string>();
+
+  logBuffer
+    .pipe(
+      //
+      bufferTime(100),
+      map((v) => v.join('\n')),
+      filter((v) => v.trim() !== ''),
+    )
+    .subscribe((message) => {
+      console.info(message);
+    });
 
   defer(() =>
     loadSecrets<{ access_key: string; secret_key: string }>({
@@ -37,7 +53,17 @@ if (cluster.isPrimary) {
                   SECRET_KEY: account.decrypted_data.secret_key,
                   WRITE_QUOTE_TO_SQL: account.secret.public_data.write_quote_to_sql ? 'true' : 'false',
                   TERMINAL_ID: `@yuants/vendor-huobi/worker/${account.secret.public_data.name}`,
+                  WORKER_LABEL: account.secret.public_data.name,
                 });
+
+                worker.on('message', (message: any) => {
+                  if (message.type === 'log') {
+                    logBuffer.next(
+                      `[Worker ${worker.process.pid} ${message.label}] ${message.level}: ${message.message}`,
+                    );
+                  }
+                });
+
                 worker.on('exit', (code, signal) => {
                   if (code === 0) {
                     console.log(`Worker ${worker.process.pid} exited gracefully`);
