267338538fbbebe7fb317d1b58ea37a4e4cb0b42
Zheng Chen <zheng.chen@no-trade-no.life>
2025-08-21 20:48:43 +0800

    feat: update environment configuration and resource limits in extension (#1402)

--- apps/node-unit/src/extension.ts
+++ apps/node-unit/src/extension.ts
@@ -9,25 +9,9 @@ export default (context: IExtensionContext) => {
         env: {
           type: 'object',
           properties: {
+            HOST_URL: { type: 'string', title: 'Host URL' },
             HOST_TOKEN: { type: 'string', title: 'for client authentication' },
             POSTGRES_URI: { type: 'string', title: 'Postgres URI' },
-          },
-        },
-        network: {
-          type: 'object',
-          properties: {
-            backward_proxy: {
-              type: 'object',
-              properties: {
-                host: { type: 'string' },
-              },
-            },
-            port_forward: {
-              type: 'object',
-              properties: {
-                host: { type: 'number' },
-              },
-            },
           },
         },
       },
@@ -35,9 +19,6 @@ export default (context: IExtensionContext) => {
     make_docker_compose_file: async (ctx, envCtx) => ({
       [COMPONENT_NAME]: {
         image: `ghcr.io/no-trade-no-life/${COMPONENT_NAME}:${ctx.version ?? envCtx.version}`,
-        ports: [['host', 8888]]
-          .filter(([name]) => ctx.network?.port_forward?.[name] !== undefined)
-          .map(([name, targetPort]) => `${ctx.network!.port_forward![name]}:${targetPort}`),
         environment: makeDockerEnvs(ctx.env),
       },
     }),
@@ -69,24 +50,6 @@ export default (context: IExtensionContext) => {
               },
             },
             spec: {
-              affinity: {
-                nodeAffinity: {
-                  preferredDuringSchedulingIgnoredDuringExecution: [
-                    {
-                      weight: 50,
-                      preference: {
-                        matchExpressions: [
-                          {
-                            key: 'node-role.kubernetes.io/spot-worker',
-                            operator: 'DoesNotExist',
-                          },
-                        ],
-                      },
-                    },
-                  ],
-                },
-              },
-              priorityClassName: 'system-cluster-critical',
               imagePullSecrets: [
                 {
                   name: 'pull-secret', // TODO(wsy): such things should be managed at namespace scope
@@ -98,109 +61,20 @@ export default (context: IExtensionContext) => {
                   image: `ghcr.io/no-trade-no-life/${COMPONENT_NAME}:${ctx.version ?? envCtx.version}`,
                   imagePullPolicy: 'IfNotPresent',
                   env: makeK8sEnvs(ctx.env),
-                  ports: [
-                    {
-                      name: 'host',
-                      containerPort: 8888,
-                      protocol: 'TCP',
-                    },
-                  ],
                   resources: {
                     limits: {
-                      cpu: ctx.cpu?.max ?? '400m',
-                      memory: ctx.memory?.max ?? '512Mi',
+                      cpu: ctx.cpu?.max ?? '1',
+                      memory: ctx.memory?.max ?? '1Gi',
                     },
                     requests: {
-                      cpu: ctx.cpu?.min ?? '100m',
-                      memory: ctx.memory?.min ?? '128Mi',
+                      cpu: ctx.cpu?.min ?? '1',
+                      memory: ctx.memory?.min ?? '1Gi',
                     },
                   },
                 },
               ],
             },
           },
-        },
-      },
-      service: {
-        apiVersion: 'v1',
-        kind: 'Service',
-        metadata: {
-          name: COMPONENT_NAME,
-          namespace: 'yuan',
-          labels: {
-            'y.ntnl.io/version': ctx.version ?? envCtx.version,
-            'y.ntnl.io/component': COMPONENT_NAME,
-          },
-        },
-        spec: {
-          type: 'ClusterIP',
-          ports: ['host']
-            .map((name) => ({
-              port: ctx.network?.port_forward?.[name],
-              targetPort: name,
-              name,
-              protocol: 'TCP',
-            }))
-            .filter(({ port }) => port != undefined),
-          selector: {
-            'y.ntnl.io/component': COMPONENT_NAME,
-          },
-        },
-      },
-      ingress: {
-        apiVersion: 'networking.k8s.io/v1',
-        kind: 'Ingress',
-        metadata: {
-          name: COMPONENT_NAME,
-          namespace: 'yuan',
-          annotations: {
-            'cert-manager.io/cluster-issuer': 'letsencrypt-prod', // TODO(wsy): make this a dependent value
-            // 'nginx.ingress.kubernetes.io/auth-signin': `https://${
-            //   ctx.annotations!.sso_url
-            // }/oauth2/start?rd=$scheme://$best_http_host$request_uri`,
-            // 'nginx.ingress.kubernetes.io/auth-url': `https://${ctx.annotations!.sso_url}/oauth2/auth`
-          },
-          labels: {
-            'y.ntnl.io/version': ctx.version ?? envCtx.version,
-            'y.ntnl.io/component': COMPONENT_NAME,
-          },
-        },
-        spec: {
-          ingressClassName: 'nginx',
-          rules: ['host']
-            .filter(
-              (v) =>
-                ctx.network?.backward_proxy?.[v] !== undefined &&
-                ctx.network?.port_forward?.[v] !== undefined,
-            )
-            .map((name) => {
-              const parts = new URL(ctx.network!.backward_proxy![name]);
-              return {
-                host: parts.host!,
-                http: {
-                  paths: [
-                    {
-                      path: parts.pathname,
-                      pathType: 'Prefix',
-                      backend: {
-                        service: {
-                          name,
-                          port: {
-                            number: ctx.network!.port_forward![name],
-                          },
-                        },
-                      },
-                    },
-                  ],
-                },
-              };
-            }),
-          tls: [
-            {
-              hosts: Object.values(ctx.network!.backward_proxy ?? {}).map((v) => new URL(v).host),
-              secretName: 'host-tls',
-            },
-          ],
         },
       },
     }),
