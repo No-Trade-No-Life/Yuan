1309f0faa9e107934a7e5edc963cfde7c234387b
CZ <cz@ntnl.io>
2025-08-23 02:11:18 +0800

    feat: add pnpm installation and update terminal initialization

--- apps/vendor-binance/src/terminal.ts
+++ apps/vendor-binance/src/terminal.ts
@@ -1,7 +1,3 @@
 import { Terminal } from '@yuants/protocol';
-import { UUID } from '@yuants/utils';
 
-export const terminal = new Terminal(process.env.HOST_URL!, {
-  terminal_id: process.env.TERMINAL_ID || `binance/${UUID()}`,
-  name: '@yuants/vendor-binance',
-});
+export const terminal = Terminal.fromNodeEnv();
