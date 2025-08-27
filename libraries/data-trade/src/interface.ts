0450dfa847392c0d05f932a097b2d6fbe22626ca
Ryan <stayrealrhr@gmail.com>
2025-08-28 02:41:35 +0800

    feat: add trade (#1448)
    * feat: add trade
    
    
    ---------
    
    Co-authored-by: Ryan <ryan@RyandeMac-mini.local>

---
+++ libraries/data-trade/src/interface.ts
@@ -0,0 +1,61 @@
+/**
+ *
+ * @public
+ */
+export interface ITrade {
+  id: string;
+  /**
+   * Account ID
+   */
+  account_id: string;
+  /**
+   * Product ID
+   */
+  product_id: string;
+
+  /**
+   * Trade Direction
+   *
+   * - `OPEN_LONG`: Open long position
+   * - `CLOSE_LONG`: Close long position
+   * - `OPEN_SHORT`: Open short position
+   * - `CLOSE_SHORT`: Close short position
+   */
+  direction: string;
+
+  /**
+   * Traded volume
+   */
+  traded_volume: string;
+
+  /**
+   * Traded price
+   */
+  traded_price: string;
+
+  /**
+   * Traded value
+   */
+  traded_value: string;
+
+  /**
+   * Fee
+   */
+  fee: string;
+
+  /**
+   * Fee currency
+   */
+  fee_currency: string;
+
+  /**
+   * Traded At
+   */
+  created_at?: string;
+
+  /**
+   * Last updated timestamp.
+   * 最后更新时间戳，使用 RFC-3339 格式，包含时区信息
+   */
+  updated_at?: string;
+}
