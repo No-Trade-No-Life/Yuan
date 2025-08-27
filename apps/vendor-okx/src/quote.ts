54ff77e097d3394759a111ae35a68054911c548c
Siyuan Wang <c.one@thrimbda.com>
2025-08-28 00:54:49 +0800

    feat(okx): publish quote channel

--- apps/vendor-okx/src/quote.ts
+++ apps/vendor-okx/src/quote.ts
@@ -1,13 +1,37 @@
-import { encodePath } from '@yuants/utils';
 import { IQuote } from '@yuants/data-quote';
 import { Terminal } from '@yuants/protocol';
 import { writeToSQL } from '@yuants/sql';
-import { defer, from, groupBy, map, merge, mergeMap, repeat, retry, scan, shareReplay, toArray } from 'rxjs';
+import { decodePath, encodePath } from '@yuants/utils';
+import {
+  defer,
+  filter,
+  from,
+  groupBy,
+  map,
+  merge,
+  mergeMap,
+  Observable,
+  repeat,
+  retry,
+  scan,
+  shareReplay,
+  toArray,
+} from 'rxjs';
 import { client } from './api';
+import { OKXWsClient } from './websocket';
 
-const swapTickers$ = defer(() => client.getMarketTickers({ instType: 'SWAP' })).pipe(
-  repeat({ delay: 5000 }),
-  retry({ delay: 5000 }),
+const wsClient = new OKXWsClient('ws/v5/public');
+
+const swapInstruments$ = defer(() => client.getInstruments({ instType: 'SWAP' })).pipe(
+  repeat({ delay: 3600_000 }),
+  retry({ delay: 10_000 }),
+  map((x) => x.data),
+  shareReplay(1),
+);
+const spotInstruments$ = defer(() => client.getInstruments({ instType: 'SPOT' })).pipe(
+  repeat({ delay: 3600_000 }),
+  retry({ delay: 10_000 }),
+  map((x) => x.data),
   shareReplay(1),
 );
 
@@ -17,19 +41,7 @@ const spotTickers$ = defer(() => client.getMarketTickers({ instType: 'SPOT' })).
   shareReplay(1),
 );
 
-export const swapMarketTickers$ = defer(() => swapTickers$).pipe(
-  mergeMap((x) =>
-    from(x.data).pipe(
-      map((x) => [x.instId, x] as const),
-      toArray(),
-      map((x) => Object.fromEntries(x)),
-    ),
-  ),
-  repeat({ delay: 5000 }),
-  retry({ delay: 5000 }),
-  shareReplay(1),
-);
-
+// depend by SubmitOrders
 export const spotMarketTickers$ = defer(() => spotTickers$).pipe(
   mergeMap((x) =>
     from(x.data).pipe(
@@ -43,48 +55,80 @@ export const spotMarketTickers$ = defer(() => spotTickers$).pipe(
   shareReplay(1),
 );
 
-const quote1$ = swapTickers$.pipe(
-  mergeMap((x) => x.data || []),
-  map(
-    (ticker): Partial<IQuote> => ({
-      datasource_id: 'OKX',
-      product_id: encodePath('SWAP', ticker.instId),
-      last_price: ticker.last,
-      ask_price: ticker.askPx,
-      bid_price: ticker.bidPx,
-      ask_volume: ticker.askSz,
-      bid_volume: ticker.bidSz,
-    }),
-  ),
+const swapTicker$ = swapInstruments$.pipe(
+  mergeMap((x) => {
+    return new Observable<any>((subscriber) => {
+      for (const inst of x) {
+        wsClient.subscribe('tickers', inst.instId, (data: any) => {
+          subscriber.next(data);
+        });
+      }
+    });
+  }),
+);
+const spotTicker$ = spotInstruments$.pipe(
+  mergeMap((x) => {
+    return new Observable<any>((subscriber) => {
+      for (const inst of x) {
+        wsClient.subscribe('tickers', inst.instId, (data: any) => {
+          subscriber.next(data);
+        });
+      }
+    });
+  }),
 );
 
-const quote2$ = spotTickers$.pipe(
-  mergeMap((x) => x.data || []),
-  map(
-    (ticker): Partial<IQuote> => ({
-      datasource_id: 'OKX',
-      product_id: encodePath('SPOT', ticker.instId),
-      last_price: ticker.last,
-      ask_price: ticker.askPx,
-      bid_price: ticker.bidPx,
-      ask_volume: ticker.askSz,
-      bid_volume: ticker.bidSz,
-    }),
-  ),
+const quote1$ = swapTicker$.pipe(
+  map((ticker) => ({
+    datasource_id: 'OKX',
+    product_id: encodePath('SWAP', ticker.instId),
+    last_price: ticker.last,
+    ask_price: ticker.askPx,
+    bid_price: ticker.bidPx,
+    ask_volume: ticker.askSz,
+    bid_volume: ticker.bidSz,
+  })),
+);
+const quote2$ = spotTicker$.pipe(
+  map((ticker) => ({
+    datasource_id: 'OKX',
+    product_id: encodePath('SPOT', ticker.instId),
+    last_price: ticker.last,
+    ask_price: ticker.askPx,
+    bid_price: ticker.bidPx,
+    ask_volume: ticker.askSz,
+    bid_volume: ticker.bidSz,
+  })),
 );
 
-const quote4$ = spotTickers$.pipe(
-  mergeMap((x) => x.data || []),
-  map(
-    (ticker): Partial<IQuote> => ({
-      datasource_id: 'OKX',
-      product_id: encodePath('MARGIN', ticker.instId),
-      last_price: ticker.last,
-      ask_price: ticker.askPx,
-      bid_price: ticker.bidPx,
-      ask_volume: ticker.askSz,
-      bid_volume: ticker.bidSz,
-    }),
+const quote3$ = spotTicker$.pipe(
+  map((ticker) => ({
+    datasource_id: 'OKX',
+    product_id: encodePath('MARGIN', ticker.instId),
+    last_price: ticker.last,
+    ask_price: ticker.askPx,
+    bid_price: ticker.bidPx,
+    ask_volume: ticker.askSz,
+    bid_volume: ticker.bidSz,
+  })),
+);
+
+const swapOpenInterestInner$ = swapInstruments$.pipe(
+  mergeMap(
+    (x) =>
+      new Observable<any>((subscriber) => {
+        for (const inst of x) {
+          wsClient.subscribe('open-interest', inst.instId, (data: any) => {
+            console.info(data);
+            const quote: Partial<IQuote> = {
+              datasource_id: 'OKX',
+              product_id: encodePath('SWAP', data.instId),
+              open_interest: data.oi,
+            };
+            subscriber.next(quote);
+          });
+        }
+      }),
   ),
 );
 
@@ -94,8 +138,7 @@ const swapOpenInterests$ = defer(() => client.getOpenInterest({ instType: 'SWAP'
   shareReplay(1),
 );
 
-const quote3$ = swapOpenInterests$.pipe(
-  mergeMap((x) => x.data || []),
+const quote4$ = swapOpenInterestInner$.pipe(
   map(
     (x): Partial<IQuote> => ({
       datasource_id: 'OKX',
@@ -104,6 +147,24 @@ const quote3$ = swapOpenInterests$.pipe(
     }),
   ),
 );
+
+const quote$ = merge(quote1$, quote2$, quote3$, quote4$).pipe(
+  groupBy((x) => encodePath(x.datasource_id, x.product_id)),
+  mergeMap((group$) => {
+    return group$.pipe(scan((acc, cur) => Object.assign(acc, cur), {} as Partial<IQuote>));
+  }),
+);
+
+Terminal.fromNodeEnv().channel.publishChannel('quote', { pattern: `^OKX/` }, (channel_id) => {
+  const [datasource_id, product_id] = decodePath(channel_id);
+  if (!datasource_id) {
+    throw 'datasource_id is required';
+  }
+  if (!product_id) {
+    throw 'product_id is required';
+  }
+  return quote$.pipe(filter((x) => x.product_id === product_id));
+});
 
 // 合并不同来源的数据并进行合并，避免死锁
 if (process.env.WRITE_QUOTE_TO_SQL === 'true') {
@@ -125,6 +186,6 @@ if (process.env.WRITE_QUOTE_TO_SQL === 'true') {
 }
 
 export const swapOpenInterest$ = defer(() => swapOpenInterests$).pipe(
-  map((x) => new Map(x.data.map((x) => [x.instId, +x.oi] as const))),
+  map((x) => new Map(x.data.map((x: any) => [x.instId, +x.oi] as const))),
   shareReplay(1),
 );
