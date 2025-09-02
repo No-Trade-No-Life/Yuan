6fd8bda14898bf4548b52230faa63b2d290bb4ae
Zheng Chen <zheng.chen@no-trade-no.life>
2025-09-03 06:21:48 +0800

    feat: refactor host manager to support TLS and simplify connection handling (#1509)

--- apps/host/src/index.ts
+++ apps/host/src/index.ts
@@ -1,20 +1,3 @@
-import { verifyMessage } from '@yuants/utils';
 import { createNodeJSHostManager } from './host-manager';
 
-createNodeJSHostManager({
-  mapHostUrlToHostId: (host_url) => {
-    const url = new URL(host_url);
-    if (process.env.HOST_TOKEN) {
-      if (process.env.HOST_TOKEN !== url.searchParams.get('host_token')) throw new Error('InvalidHostToken');
-    }
-    if (process.env.MULTI_TENANCY === 'ED25519') {
-      const public_key = url.searchParams.get('public_key')!;
-      const signature = url.searchParams.get('signature')!;
-      if (!public_key) throw new Error('public_key is required');
-      if (!signature) throw new Error('signature is required');
-      if (!verifyMessage('', signature, public_key)) throw new Error('signature is invalid');
-      return public_key;
-    }
-    return 'main';
-  },
-});
+createNodeJSHostManager();
