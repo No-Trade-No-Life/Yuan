9d0254ba935431d652bd8a1b494917aa7c2c20c5
Zheng Chen <zheng.chen@no-trade-no.life>
2025-09-05 12:29:42 +0800

    feat: enhance post-install script for Debian 12 support and add changelog entry (#1533)

--- apps/vendor-ctp/scripts/post-install.js
+++ apps/vendor-ctp/scripts/post-install.js
@@ -6,36 +6,41 @@ const { arch } = require('os');
 if (process.platform === 'linux') {
   if (arch() === 'x64') {
     console.log('Detected Linux x64 architecture.');
-    // if is apt-get based system
-    if (execSync('which apt || echo ""').toString().trim()) {
+    // check Debian 12 (bookworm)
+    const isDebian12 =
+      execSync('cat /etc/os-release | grep "VERSION_ID=\\"12\\"" || echo ""').toString().trim() !== '';
+    if (isDebian12) {
       console.log('Detected apt-get based system. Installing dependencies...');
-      execSync('apt update && apt install -y libspdlog-dev libzmq3-dev', {
+      execSync(
+        'apt-get update && apt-get install -y gcc cmake make libspdlog-dev libzmq3-dev wget git build-essential',
+        {
+          stdio: 'inherit',
+        },
+      );
+
+      // Install cppzmq
+      const installZmqDir = join(tmpdir(), 'ctp/cppzmq', UUID());
+      console.info('Installing cppzmq in', installZmqDir);
+      mkdirSync(installZmqDir, { recursive: true });
+      execSync(
+        `wget -O - https://github.com/zeromq/cppzmq/archive/refs/tags/v4.9.0.tar.gz | tar -zxv -C ${installZmqDir}`,
+        { stdio: 'inherit' },
+      );
+      mkdirSync(join(installZmqDir, 'cppzmq-4.9.0', 'build'));
+      execSync('cmake .. && make -j4 install', {
+        cwd: join(installZmqDir, 'cppzmq-4.9.0', 'build'),
         stdio: 'inherit',
       });
+      rmSync(installZmqDir, { recursive: true, force: true });
+      console.log('cppzmq installed successfully.');
 
-      //   // Install cppzmq
-      //   const installZmqDir = join(tmpdir(), 'ctp/cppzmq', UUID());
-      //   console.info('Installing cppzmq in', installZmqDir);
-      //   mkdirSync(installZmqDir, { recursive: true });
-      //   execSync(
-      //     `wget -O - https://github.com/zeromq/cppzmq/archive/refs/tags/v4.9.0.tar.gz | tar -zxv -C ${installZmqDir}`,
-      //     { stdio: 'inherit' },
-      //   );
-      //   mkdirSync(join(installZmqDir, 'cppzmq-4.9.0', 'build'));
-      //   execSync('cmake .. && make -j4 install', {
-      //     cwd: join(installZmqDir, 'cppzmq-4.9.0', 'build'),
-      //     stdio: 'inherit',
-      //   });
-      //   rmSync(installZmqDir, { recursive: true, force: true });
-      //   console.log('cppzmq installed successfully.');
-
-      //   // Build CTP (main_linux)
-      //   console.log('Building CTP native addons...');
-      //   const ctpBuildDir = join(__dirname, '../../ctp/build');
-      //   rmSync(ctpBuildDir, { recursive: true, force: true });
-      //   mkdirSync(ctpBuildDir, { recursive: true });
-      //   execSync('cmake .. && make', { cwd: ctpBuildDir, stdio: 'inherit' });
-      //   console.log('CTP native addons built successfully.');
+      // Build CTP (main_linux)
+      console.log('Building CTP native addons...');
+      const ctpBuildDir = join(__dirname, '../../ctp/build');
+      rmSync(ctpBuildDir, { recursive: true, force: true });
+      mkdirSync(ctpBuildDir, { recursive: true });
+      execSync('cmake .. && make', { cwd: ctpBuildDir, stdio: 'inherit' });
+      console.log('CTP native addons built successfully.');
       process.exit(0);
     }
   }
