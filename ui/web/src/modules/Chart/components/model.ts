e9741761f07c9d67f0d6736400397b093e4fc135
Zheng Chen <zheng.chen@no-trade-no.life>
2025-09-11 07:12:54 +0800

    feat: implement custom series handling and legend rendering for chart components (#1575)

--- ui/web/src/modules/Chart/components/model.ts
+++ ui/web/src/modules/Chart/components/model.ts
@@ -1,3 +1,6 @@
+import { IChartApi } from 'lightweight-charts';
+import { BehaviorSubject, Observable } from 'rxjs';
+
 export interface ILoadedData {
   filename: string;
   data_index: number;
@@ -9,4 +12,29 @@ export interface ILoadedData {
 export interface IDataRef {
   data_index: number;
   column_name: string;
+}
+
+export interface ICustomSeries {
+  type: string;
+  addSeries: (ctx: {
+    //
+    chart: IChartApi;
+    paneIndex: number;
+    seriesIndex: number;
+    seriesConfig: {
+      type: string;
+      refs: IDataRef[];
+    };
+    dataSeries: any[][];
+    timeLine: [time: number, index: number][];
+    cursor$: BehaviorSubject<number | undefined>;
+    viewStartIndex: number;
+    dispose$: Observable<void>;
+  }) => void;
+  Legend: React.ComponentType<{
+    seriesConfig: { type: string; refs: IDataRef[] };
+    seriesIndex: number;
+    globalDataSeries: any[][];
+    cursorIndex: number;
+  }>;
 }
