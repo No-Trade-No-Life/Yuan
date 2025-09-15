ccbd0cf2509459bb48aa0f54ca99a1df759d3302
Ryan <ryan@RyandeMac-mini.local>
2025-09-16 01:19:34 +0800

    feat: add order

--- apps/vendor-okx/src/services.ts
+++ apps/vendor-okx/src/services.ts
@@ -1,12 +1,26 @@
 import { Terminal } from '@yuants/protocol';
 import { client } from './api';
+import { defer, firstValueFrom } from 'rxjs';
+import { tradingAccountInfo$ } from './account';
 
 const terminal = Terminal.fromNodeEnv();
 
-terminal.provideService('Grid/Algo-Order', {}, async (msg) => {
-  if (msg.req) {
-    const result = await client.postGridAlgoOrder(msg.req as any);
-    return { res: { code: 0, message: 'OK', data: result } };
-  }
-  return { res: { code: 0, message: 'No Params' } };
-});
+defer(async () => {
+  const tradingAccountInfo = await firstValueFrom(tradingAccountInfo$);
+  terminal.provideService(
+    'Grid/Algo-Order',
+    {
+      required: ['account_id'],
+      properties: {
+        account_id: { const: tradingAccountInfo.account_id },
+      },
+    },
+    async (msg) => {
+      if (msg.req) {
+        const result = await client.postGridAlgoOrder(msg.req as any);
+        return { res: { code: 0, message: 'OK', data: result } };
+      }
+      return { res: { code: 0, message: 'No Params' } };
+    },
+  );
+}).subscribe();
