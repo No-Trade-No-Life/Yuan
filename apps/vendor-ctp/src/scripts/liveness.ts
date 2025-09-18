3faa396e1a475d05b3c8f916d6845490e61b2b45
Zheng Chen <zheng.chen@no-trade-no.life>
2025-09-19 03:00:11 +0800

    refactor: reorganize requestZMQ functionality and update liveness check (#1657)

--- apps/vendor-ctp/src/scripts/liveness.ts
+++ apps/vendor-ctp/src/scripts/liveness.ts
@@ -1,12 +1,12 @@
 import { catchError, defer, filter, map, of, retry, throwIfEmpty, timeout } from 'rxjs';
-import { requestZMQ } from '../exchange';
 import { createZMQConnection } from '../bridge';
+import { requestZMQ } from '../requestZMQ';
 
 process.env.ZMQ_PULL_URL = 'tcp://localhost:5700';
 process.env.ZMQ_PUSH_URL = 'tcp://*:5702';
 const conn = createZMQConnection(process.env.ZMQ_PUSH_URL!, process.env.ZMQ_PULL_URL!);
 
-const liveness$ = defer(() => requestZMQ(conn, { method: 'Ping', params: {} })).pipe(
+const liveness$ = defer(() => requestZMQ({ method: 'Ping', params: {} })).pipe(
   //
   map((v) => v.res?.error_code),
   timeout(3000),
