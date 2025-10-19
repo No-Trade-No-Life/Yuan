c8a85aa73f3dd8a89b895ded0ecb601e708615ab
CZ <cz@ntnl.io>
2025-10-20 06:53:49 +0800

    feat: enhance TerminalSecurity module by implementing encryption and decryption with remote public keys

--- libraries/protocol/src/security.ts
+++ libraries/protocol/src/security.ts
@@ -1,6 +1,8 @@
 import {
+  decrypt,
   deriveSharedKey,
   encodePath,
+  encrypt,
   generateX25519KeyPair,
   signMessage,
   verifyMessage,
@@ -14,9 +16,9 @@ import { Terminal } from './terminal';
  */
 export class TerminalSecurity {
   /**
-   * 保存远程生成的 X25519 公钥到共享密钥的映射
+   * 保存远程 ED25519 公钥到共享密钥的映射
    */
-  mapX25519PublicKeyToSharedKey = new Map<string, string>();
+  mapRemotePublicKeyToSharedKey = new Map<string, string>();
 
   constructor(public readonly terminal: Terminal) {
     setTimeout(() => this._setupHandShakeService());
@@ -25,6 +27,8 @@ export class TerminalSecurity {
   private _setupHandShakeService() {
     this.terminal.server.provideService<
       {
+        public_key: string;
+        signature: string;
         x25519_public_key: string;
       },
       {
@@ -40,11 +44,16 @@ export class TerminalSecurity {
           x25519_public_key: { type: 'string' },
         },
       },
-      async ({ req: { x25519_public_key } }) => {
+      async ({ req: { x25519_public_key, public_key, signature } }) => {
+        if (!verifyMessage(x25519_public_key, signature, public_key)) {
+          throw new Error('Invalid signature');
+        }
+
         const localKeyPair = generateX25519KeyPair();
 
         const sharedKey = deriveSharedKey(x25519_public_key, localKeyPair.private_key);
-        this.mapX25519PublicKeyToSharedKey.set(x25519_public_key, sharedKey);
+
+        this.mapRemotePublicKeyToSharedKey.set(public_key, sharedKey);
 
         const message = `${x25519_public_key}${localKeyPair.public_key}`;
 
@@ -67,6 +76,8 @@ export class TerminalSecurity {
 
     const data = await this.terminal.client.requestForResponseData<
       {
+        public_key: string;
+        signature: string;
         x25519_public_key: string;
       },
       {
@@ -75,6 +86,8 @@ export class TerminalSecurity {
       }
     >(encodePath('HandShake', ed25519_public_key), {
       x25519_public_key: myPair.public_key,
+      public_key: this.terminal.keyPair.public_key,
+      signature: signMessage(myPair.public_key, this.terminal.keyPair.private_key),
     });
 
     if (!verifyMessage(`${myPair.public_key}${data.x25519_public_key}`, data.signature, ed25519_public_key)) {
@@ -82,24 +95,50 @@ export class TerminalSecurity {
     }
 
     const shared_key = deriveSharedKey(data.x25519_public_key, myPair.private_key);
+    this.mapRemotePublicKeyToSharedKey.set(ed25519_public_key, shared_key);
+  }
 
-    return {
-      /**
-       * Local X25519 public key (base58)
-       */
-      public_key: myPair.public_key,
-      /**
-       * Local X25519 private key (base58)
-       */
-      private_key: myPair.private_key,
-      /**
-       * Remote X25519 public key (base58)
-       */
-      remote_public_key: data.x25519_public_key,
-      /**
-       * AES-GCM shared key (base58)
-       */
-      shared_key: shared_key,
-    };
+  /**
+   * Decrypt data with remote ED25519 public key
+   *
+   * @param remote_public_key - remote ED25519 public key
+   * @param encrypted_data - encrypted data (Uint8Array)
+   * @returns - decrypted data (Uint8Array)
+   *
+   * @public
+   */
+  async decryptDataWithRemotePublicKey(
+    encrypted_data: Uint8Array,
+    remote_public_key: string,
+  ): Promise<Uint8Array> {
+    // 数据已经加密，说明交换过共享密钥
+    try {
+      // 乐观估计密钥有效
+      const shared_key = this.mapRemotePublicKeyToSharedKey.get(remote_public_key);
+      if (!shared_key) throw new Error(`LocalSharedKeyNotFound: remote_public_key=${remote_public_key}`);
+      return decrypt(encrypted_data, shared_key);
+    } catch (err) {
+      // 可能是密钥无效，重新请求共享密钥 (不阻塞)
+      this.requestSharedKey(remote_public_key);
+      throw err;
+    }
+  }
+
+  /**
+   * Encrypt data with remote public key
+   * @param data - Data to encrypt
+   * @param remote_public_key - Remote public key
+   * @returns - Encrypted data (Uint8Array)
+   *
+   * @public
+   */
+  async encryptDataWithRemotePublicKey(data: Uint8Array, remote_public_key: string): Promise<Uint8Array> {
+    let shared_key = this.mapRemotePublicKeyToSharedKey.get(remote_public_key);
+    if (!shared_key) {
+      // 主动请求共享密钥
+      await this.requestSharedKey(remote_public_key);
+      shared_key = this.mapRemotePublicKeyToSharedKey.get(remote_public_key)!;
+    }
+    return encrypt(data, shared_key);
   }
 }
