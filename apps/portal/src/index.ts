1c362a47079e89218555154c858c6719e9bfc938
Zheng Chen <zheng.chen@no-trade-no.life>
2025-10-19 21:43:56 +0800

    feat: rewrite and support filter schema and external terminal disposing (#1893)

--- apps/portal/src/index.ts
+++ apps/portal/src/index.ts
@@ -1,128 +1,142 @@
-import { Terminal } from '@yuants/protocol';
+import { IServiceInfo, Terminal } from '@yuants/protocol';
 import { requestSQL } from '@yuants/sql';
 import { formatTime, listWatch, UUID } from '@yuants/utils';
+import Ajv from 'ajv';
 import {
-  combineLatest,
+  debounceTime,
   defer,
   filter,
   from,
-  groupBy,
-  map,
   mergeMap,
   Observable,
   repeat,
+  ReplaySubject,
   retry,
-  shareReplay,
+  share,
+  switchMap,
   tap,
   toArray,
 } from 'rxjs';
 
-const internalTerminal = new Terminal(process.env.HOST_URL!, {
-  terminal_id: `Portal/Internal/${UUID()}`,
-  enable_WebRTC: process.env.ENABLE_WEBRTC === 'true',
-  name: 'Portal-Internal',
-});
+const internalTerminal = Terminal.fromNodeEnv();
 
-interface IPortalRelation {
+interface IPortalConfig {
+  id: string;
   external_host_url: string;
-  type: 'request' | 'channel';
-  direction: 'export' | 'import';
-  method?: string;
-  schema: any;
+  is_import: boolean;
+  filter_schema: any;
+  enabled: boolean;
+  created_at: string;
+  updated_at: string;
 }
 
-const portalTerminals$ = from(internalTerminal.terminalInfos$).pipe(
-  mergeMap((x) =>
-    from(x).pipe(
-      //
-      filter((xx) => xx.name === internalTerminal.terminalInfo.name),
-      map((xx) => xx.terminal_id),
-      toArray(),
-      filter((x) => x.length > 0 && x.some((xx) => xx === internalTerminal.terminal_id)), // ISSUE: ignore setup-phrase no self
+const cacheTerminal$: Record<string, Observable<Terminal>> = {};
+const useTerminal = (hostUrl: string) =>
+  (cacheTerminal$[hostUrl] ??= new Observable<Terminal>((sub) => {
+    const terminal = new Terminal(
+      hostUrl,
+      {
+        terminal_id: `Portal/External/${UUID()}`,
+        name: 'Portal-External',
+      },
+      { disableTerminate: true, disableMetrics: true },
+    );
+    sub.next(terminal);
+    sub.add(() => {
+      terminal.dispose();
+    });
+  }).pipe(
+    //
+    share({ resetOnRefCountZero: true, connector: () => new ReplaySubject(1) }),
+  ));
+
+const runService = (serviceInfo: IServiceInfo, target: Terminal, source: Terminal) =>
+  new Observable((sub) => {
+    const service = target.server.provideService(serviceInfo.method, serviceInfo.schema, (msg) =>
+      source.client.requestService(serviceInfo.method, msg.req),
+    );
+    sub.add(() => {
+      service.dispose();
+    });
+  }).pipe(
+    tap({
+      subscribe: () =>
+        console.info(
+          formatTime(Date.now()),
+          'RunService',
+          `service_id=${serviceInfo.service_id}, target=${target.terminal_id}, source=${source.terminal_id}`,
+        ),
+      finalize: () =>
+        console.info(
+          formatTime(Date.now()),
+          'StopService',
+          `service_id=${serviceInfo.service_id}, target=${target.terminal_id}, source=${source.terminal_id}`,
+        ),
+    }),
+  );
+
+const filterServiceInfoList = (
+  terminal: Terminal,
+  validator: (serviceInfo: IServiceInfo) => boolean,
+): Observable<IServiceInfo[]> =>
+  terminal.terminalInfos$.pipe(
+    debounceTime(500),
+    switchMap((terminalInfos) =>
+      from(terminalInfos).pipe(
+        mergeMap((terminalInfo) =>
+          from(Object.values(terminalInfo.serviceInfo || {})).pipe(
+            filter((serviceInfo) => validator(serviceInfo)),
+          ),
+        ),
+        toArray(),
+      ),
     ),
-  ),
-  shareReplay(1),
-  tap((arr) => console.info(formatTime(Date.now()), 'FindPortals', arr.length)),
-);
+  );
 
-const portalRelation$ = defer(() =>
-  requestSQL<IPortalRelation[]>(internalTerminal, `select * from portal_relation order by updated_at`),
-).pipe(
-  //
-  retry({ delay: 5000 }),
-  repeat({ delay: 30_000 }),
-  shareReplay(1),
-  tap((arr) => console.info(formatTime(Date.now()), 'FetchedPortalRelations', arr.length)),
-);
+const runPortalConfig = (config: IPortalConfig): Observable<any> => {
+  console.info(formatTime(Date.now()), `RunPortalConfig: ${config.id}`);
+  const validator = new Ajv({ strict: false, strictSchema: false }).compile(config.filter_schema);
 
-const portalRelationsGroupByExternalHost$ = portalRelation$.pipe(
-  mergeMap((x) =>
-    from(x).pipe(
-      groupBy((x) => x.external_host_url),
-      mergeMap((group) =>
-        group.pipe(
-          //
-          toArray(),
-          map((v) => ({ host_url: group.key, items: v })),
+  if (config.is_import) {
+    return useTerminal(config.external_host_url).pipe(
+      switchMap((externalTerminal) =>
+        filterServiceInfoList(externalTerminal, validator).pipe(
+          listWatch(
+            (x) => x.service_id,
+            (x) => runService(x, internalTerminal, externalTerminal),
+          ),
         ),
       ),
-      toArray(),
-    ),
-  ),
-);
+    );
+  }
 
-combineLatest([portalRelationsGroupByExternalHost$, portalTerminals$])
-  .pipe(
-    // Load balance: many portals in one host, sharing workload
-    map(([list, portals]) => {
-      const idx = portals.indexOf(internalTerminal.terminal_id);
-      const N = list.length;
-      const M = portals.length;
-      const size = Math.floor(N / M) + (N % M > idx ? 1 : 0);
-      const startIdx = Math.floor(N / M) * idx + Math.min(idx, N % M);
-      const endIdx = startIdx + size;
-      return list.slice(startIdx, endIdx);
-    }),
-  )
-  .pipe(
-    tap((arr) => console.info(formatTime(Date.now()), 'OwnsHostsCount', arr.length)),
+  return filterServiceInfoList(internalTerminal, validator).pipe(
     listWatch(
-      (x) => x.host_url,
-      (x) => {
-        return new Observable<{ externalTerminal: Terminal; services: IPortalRelation[] }>((subscriber) => {
-          console.info(formatTime(Date.now()), `SetupExternalTerminal: ${x.host_url}`);
-          const terminal = new Terminal(
-            x.host_url,
-            {
-              terminal_id: `Portal/External/${UUID()}`,
-              name: 'Portal-External',
-            },
-            { disableTerminate: true, disableMetrics: true },
-          );
-          subscriber.next({ externalTerminal: terminal, services: x.items });
-          return () => {
-            console.info(formatTime(Date.now()), `DisposeExternalTerminal: ${x.host_url}`);
-            terminal.dispose();
-          };
-        });
-      },
-      (a, b) => JSON.stringify(a) === JSON.stringify(b),
+      (x) => x.service_id,
+      (x) =>
+        useTerminal(config.external_host_url).pipe(
+          switchMap((externalTerminal) => runService(x, externalTerminal, internalTerminal)),
+        ),
     ),
-    tap(({ externalTerminal, services }) => {
-      console.info(formatTime(Date.now()), `SetupServices: ${externalTerminal.host_url}`);
-      for (const service of services) {
-        const [fromTerminal, toTerminal] =
-          service.direction === 'export'
-            ? [internalTerminal, externalTerminal]
-            : [externalTerminal, internalTerminal];
+  );
+};
 
-        if (service.type === 'request' && service.method) {
-          console.info(formatTime(Date.now()), `SetupService: ${service.method}`);
-          toTerminal.server.provideService(service.method, service.schema, (msg) =>
-            fromTerminal.client.requestService(msg.method!, msg.req),
-          );
-        }
-      }
-    }),
+defer(() => requestSQL<IPortalConfig[]>(internalTerminal, `select * from portal_config where enabled = true`))
+  .pipe(
+    repeat({ delay: 5000 }),
+    retry({ delay: 5000 }),
+    listWatch(
+      (x) => x.id,
+      (x) =>
+        defer(() => runPortalConfig(x)).pipe(
+          repeat({ delay: 1000 }),
+          retry({ delay: 1000 }),
+          tap({
+            subscribe: () => console.info(formatTime(Date.now()), 'StartPortalConfig', x.id),
+            finalize: () => console.info(formatTime(Date.now()), 'StopPortalConfig', x.id),
+          }),
+        ),
+      (a, b) => a.updated_at === b.updated_at,
+    ),
   )
   .subscribe();
