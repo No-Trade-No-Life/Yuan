2d3b14e29377eb0ec87edd2b509b3f65e6d7f537
Zheng Chen <zheng.chen@no-trade-no.life>
2025-10-20 23:57:59 +0800

    feat: implement WebSocket connection management and update dependencies (#1907)

--- apps/vendor-tq/src/common/ws.ts
+++ apps/vendor-tq/src/common/ws.ts
@@ -1,27 +1,55 @@
-import { createConnectionJson } from '@yuants/protocol';
-import { from, share, Subject, tap } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { formatTime, UUID } from '@yuants/utils';
+import { WebSocket } from 'ws';
 
 const MARKET_INFO_WS_URL = 'wss://free-api.shinnytech.com/t/nfmd/front/mobile';
 
-export const createConnectionTq = () => {
-  const conn = createConnectionJson(MARKET_INFO_WS_URL);
+const pool = new Set<WebSocket>();
 
-  const input$ = from(conn.input$).pipe(
-    tap((msg) => console.debug(new Date(), 'TQ', 'input$', JSON.stringify(msg))),
-    share(),
-  );
+export const useTQ = () =>
+  new Observable<{
+    ws: WebSocket;
+    input$: Subject<any>;
+    output$: Subject<any>;
+  }>((sub) => {
+    const ws = new WebSocket(MARKET_INFO_WS_URL);
+    const id = UUID();
 
-  const output$ = new Subject<any>();
-  output$
-    .pipe(
-      //
-      tap((msg) => console.debug(new Date(), 'TQ', 'output$', JSON.stringify(msg))),
-    )
-    .subscribe(conn.output$);
-  const connection$ = conn.connection$;
-  return {
-    input$,
-    output$,
-    connection$,
-  };
-};
+    const input$ = new Subject<any>();
+    const output$ = new Subject<any>();
+    pool.add(ws);
+
+    ws.addEventListener('open', () => {
+      console.info(formatTime(Date.now()), 'TQ_WS_OPEN', id, pool.size);
+      sub.next({ ws, input$, output$ });
+    });
+
+    ws.addEventListener('error', (err) => {
+      console.error(formatTime(Date.now()), 'TQ_WS_ERROR', id, pool.size, err);
+      sub.error(err);
+    });
+
+    ws.addEventListener('close', () => {
+      console.info(formatTime(Date.now()), 'TQ_WS_CLOSE', id, pool.size);
+      sub.complete();
+    });
+
+    ws.addEventListener('message', (event) => {
+      const data = JSON.parse(event.data.toString());
+      input$.next(data);
+    });
+
+    sub.add(() => {
+      pool.delete(ws);
+      ws.close();
+    });
+
+    const sub1 = output$.subscribe((msg) => {
+      ws.send(JSON.stringify(msg));
+    });
+
+    sub.add(() => {
+      sub1.unsubscribe();
+      output$.complete();
+    });
+  });
