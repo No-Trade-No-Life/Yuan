fb5a96d358b298318e28faa7226e69b6a9bb5a91
Siyuan Wang <c.one@thrimbda.com>
2025-11-06 14:11:09 +0800

    feat(vendor-aster): add funding-rate for aster

--- apps/vendor-aster/src/index.ts
+++ apps/vendor-aster/src/index.ts
@@ -2,6 +2,7 @@ import './account';
 import './account-spot';
 import './order';
 import './order-spot';
+import './interest_rate';
 import './product'; // Import the new product service
 import './pending-orders';
 import './quote';
