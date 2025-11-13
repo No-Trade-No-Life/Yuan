b656ea3db9e63ff865eb12b45f298276c43b8e42
Siyuan Wang <c.one@thrimbda.com>
2025-11-13 15:31:39 +0800

    feat(vendor-hyperliquid): add open interest for perps

--- apps/vendor-hyperliquid/src/api.ts
+++ apps/vendor-hyperliquid/src/api.ts
@@ -321,6 +321,36 @@ export class HyperliquidClient {
   /**
    * info
    *
+   * https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/api/info-endpoint/perpetuals#retrieve-perpetuals-asset-contexts-includes-mark-price-current-funding-open-interest-etc.
+   */
+  getMetaAndAssetCtxs = (): Promise<
+    [
+      {
+        universe: {
+          name: string;
+          szDecimals: number;
+          maxLeverage: number;
+          onlyIsolated?: boolean;
+          isDelisted?: boolean;
+        }[];
+      },
+      {
+        dayNtlVlm: string;
+        funding: string;
+        impactPxs: [string, string];
+        markPx: string;
+        midPx: string;
+        openInterest: string;
+        oraclePx: string;
+        premium: string;
+        prevDayPx: string;
+      }[],
+    ]
+  > => this.request('POST', 'info', { type: 'metaAndAssetCtxs' });
+
+  /**
+   * info
+   *
    * Only the most recent 5000 candles are available
    *
    * Supported intervals: "1m", "3m", "5m", "15m", "30m", "1h", "2h", "4h", "8h", "12h", "1d", "3d", "1w", "1M"
