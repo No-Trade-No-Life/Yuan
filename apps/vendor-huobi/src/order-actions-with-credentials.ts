deb3a2ca42a7a9ca9e1afabe6837e200475f0d66
CZ <zheng.chen@no-trade-no.life>
2025-11-15 05:03:08 +0800

    feat: Refactor order submission to enhance credential handling and response structure (#2028)

--- apps/vendor-huobi/src/order-actions-with-credentials.ts
+++ apps/vendor-huobi/src/order-actions-with-credentials.ts
@@ -1,35 +1,21 @@
-import { IOrder } from '@yuants/data-order';
+import { provideOrderActionsWithCredential } from '@yuants/data-order';
 import { Terminal } from '@yuants/protocol';
-import { ICredential } from './api/private-api';
-import { handleSuperMarginOrder, handleSwapOrder } from './orders/submitOrder';
+import { submitOrder } from './orders/submitOrder';
 
 const terminal = Terminal.fromNodeEnv();
 
-terminal.server.provideService<IOrder & { credential: ICredential }>(
-  'SubmitOrder',
+provideOrderActionsWithCredential(
+  terminal,
+  'HTX',
   {
-    required: ['account_id', 'credential'],
+    type: 'object',
+    required: ['access_key', 'secret_key'],
     properties: {
-      account_id: {
-        type: 'string',
-        pattern: '^huobi/',
-      },
-      credential: {
-        type: 'object',
-        required: ['access_key', 'secret_key'],
-        properties: {
-          access_key: { type: 'string' },
-          secret_key: { type: 'string' },
-        },
-      },
+      access_key: { type: 'string' },
+      secret_key: { type: 'string' },
     },
   },
-  async (msg) => {
-    if (/huobi\/(.*)\/swap/.test(msg.req.account_id)) {
-      await handleSwapOrder(msg.req, msg.req.credential);
-    } else if (/huobi\/(.*)\/super-margin/.test(msg.req.account_id)) {
-      await handleSuperMarginOrder(msg.req, msg.req.credential);
-    } else throw new Error(`Unsupported account_id: ${msg.req.account_id}`);
-    return { res: { code: 0, message: 'OK' } };
+  {
+    submitOrder,
   },
 );
