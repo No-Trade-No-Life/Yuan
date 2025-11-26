51d28b9985e378ca881b30f9793fac278fb88b89
Ryan <stayrealrhr@gmail.com>
2025-11-26 19:07:22 +0800

    feat: refactor gate

--- apps/vendor-gate/src/index.ts
+++ apps/vendor-gate/src/index.ts
@@ -4,3 +4,4 @@ import './services/order-actions-with-credential';
 import './services/markets/product';
 import './services/markets/interest-rate';
 import './services/transfer';
+import './services/exchange';
