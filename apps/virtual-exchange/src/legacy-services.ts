0b9ef6e5ed17282009f71b7c8eaa5ff5a0e07ffd
CZ <zheng.chen@no-trade-no.life>
2025-11-26 13:11:39 +0800

    feat: implement exchange credential services and update package dependencies (#2119)

---
+++ apps/virtual-exchange/src/legacy-services.ts
@@ -0,0 +1,127 @@
+import { provideAccountInfoService } from '@yuants/data-account';
+import { IOrder, providePendingOrdersService } from '@yuants/data-order';
+import { cancelOrder, getOrders, getPositions, modifyOrder, submitOrder } from '@yuants/exchange';
+import { Terminal } from '@yuants/protocol';
+import { listWatch, newError } from '@yuants/utils';
+import { map, Observable } from 'rxjs';
+import { validCredentials$ } from './credential';
+
+const terminal = Terminal.fromNodeEnv();
+
+validCredentials$
+  .pipe(
+    map((x) => Array.from(x.entries())),
+    listWatch(
+      ([id]) => id,
+      ([credential_id, credential]) =>
+        new Observable((sub) => {
+          console.info(`Setting up VEX services for credential: ${credential_id}`);
+          // Setup AccountInfo Service
+          {
+            const service = provideAccountInfoService(
+              terminal,
+              credential_id,
+              async () => {
+                const res = await getPositions(terminal, credential);
+                if (res.code === 0 && res.data) {
+                  return res.data;
+                }
+                throw newError('FETCH_POSITIONS_FAILED', { credential_id, reason: res.message });
+              },
+              {
+                auto_refresh_interval: 1000,
+              },
+            );
+            sub.add(() => {
+              service.dispose$.next();
+            });
+          }
+          // Setup Pending Orders Service
+          {
+            const service = providePendingOrdersService(
+              terminal,
+              credential_id,
+              async () => {
+                const res = await getOrders(terminal, credential);
+                if (!res.data) throw newError('FETCH_ORDERS_FAILED', { credential_id, res });
+
+                res.data.forEach((order) => {
+                  order.account_id = credential_id;
+                });
+
+                return res.data;
+              },
+              {
+                auto_refresh_interval: 1000,
+              },
+            );
+            sub.add(() => {
+              service.dispose$.next();
+            });
+          }
+          // Setup SubmitOrder Service
+          {
+            const service = terminal.server.provideService<IOrder, { order_id: string }>(
+              'SubmitOrder',
+              {
+                type: 'object',
+                required: ['account_id'],
+                properties: {
+                  account_id: { type: 'string', const: credential_id },
+                },
+              },
+              async (msg) => {
+                const res = await submitOrder(terminal, credential, msg.req);
+                return { res };
+              },
+            );
+            sub.add(() => {
+              service.dispose();
+            });
+          }
+
+          // Setup ModifyOrder Service
+          {
+            const service = terminal.server.provideService<IOrder, void>(
+              'ModifyOrder',
+              {
+                type: 'object',
+                required: ['account_id'],
+                properties: {
+                  account_id: { type: 'string', const: credential_id },
+                },
+              },
+              async (msg) => {
+                const res = await modifyOrder(terminal, credential, msg.req);
+                return { res };
+              },
+            );
+            sub.add(() => {
+              service.dispose();
+            });
+          }
+
+          // Setup CancelOrder Service
+          {
+            const service = terminal.server.provideService<IOrder, void>(
+              'CancelOrder',
+              {
+                type: 'object',
+                required: ['account_id'],
+                properties: {
+                  account_id: { type: 'string', const: credential_id },
+                },
+              },
+              async (msg) => {
+                const res = await cancelOrder(terminal, credential, msg.req);
+                return { res };
+              },
+            );
+            sub.add(() => {
+              service.dispose();
+            });
+          }
+        }),
+    ),
+  )
+  .subscribe();
