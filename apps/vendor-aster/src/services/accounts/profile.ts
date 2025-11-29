bbfb6fb90f8bee9fd1fa063d241b6eaedbedb36a
CZ <zheng.chen@no-trade-no.life>
2025-11-29 23:31:45 +0800

    refactor: simplify getCredentialId function to use credential address directly (#2141)

--- apps/vendor-aster/src/services/accounts/profile.ts
+++ apps/vendor-aster/src/services/accounts/profile.ts
@@ -1,10 +1,6 @@
 import { encodePath } from '@yuants/utils';
-import { getFApiV2Balance, ICredential } from '../../api/private-api';
+import { ICredential } from '../../api/private-api';
 
 export const getCredentialId = async (credential: ICredential): Promise<string> => {
-  const accountBalance = await getFApiV2Balance(credential, {});
-  if (!accountBalance?.[0]) {
-    throw new Error('No Account Balance');
-  }
-  return encodePath('ASTER', accountBalance[0].accountAlias);
+  return encodePath('ASTER', credential.address);
 };
