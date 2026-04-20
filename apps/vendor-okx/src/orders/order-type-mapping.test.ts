73b662d9ca5fe4f8fbc8697636bcac258c41a317
Ryan <stayrealrhr@gmail.com>
2026-04-20 17:29:01 +0800

    test(okx): cover ioc fok order type mappings

---
+++ apps/vendor-okx/src/orders/order-type-mapping.test.ts
@@ -0,0 +1,23 @@
+import { mapOkxOrdTypeToOrderType } from './mapOkxOrdTypeToOrderType';
+import { mapOrderTypeToOrdType } from './mapOrderTypeToOrdType';
+
+describe('mapOrderTypeToOrdType', () => {
+  test('maps IOC and FOK to OKX ordType values', () => {
+    expect(mapOrderTypeToOrdType('IOC')).toBe('ioc');
+    expect(mapOrderTypeToOrdType('FOK')).toBe('fok');
+  });
+});
+
+describe('mapOkxOrdTypeToOrderType', () => {
+  test('maps OKX ordType values back to Yuan order types', () => {
+    expect(mapOkxOrdTypeToOrderType('market')).toBe('MARKET');
+    expect(mapOkxOrdTypeToOrderType('limit')).toBe('LIMIT');
+    expect(mapOkxOrdTypeToOrderType('post_only')).toBe('MAKER');
+    expect(mapOkxOrdTypeToOrderType('ioc')).toBe('IOC');
+    expect(mapOkxOrdTypeToOrderType('fok')).toBe('FOK');
+  });
+
+  test('returns UNKNOWN for unsupported ordType', () => {
+    expect(mapOkxOrdTypeToOrderType('optimal_limit_ioc')).toBe('UNKNOWN');
+  });
+});
